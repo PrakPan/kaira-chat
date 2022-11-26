96df84c980ec4a48f22e17e6b94c2ac757feeb3f
Devansh Chawla <devansh@thetarzanway.com>
2022-11-26 20:45:55 +0530

    PW logos added

--- containers/PW/FullImgContent.js
+++ containers/PW/FullImgContent.js
@@ -5,12 +5,15 @@ import media from '../../components/media';
 import {BsWhatsapp} from 'react-icons/bs';
 import urls from '../../services/urls';
 import Enquiry from './newenquiry/Index';
-
+import ImageLoader from '../../components/ImageLoader';
+import Banner from './BannerOne';
 const Container = styled.div`
     text-align: center;
  color:white;
  width: 100%;
-@media screen and (min-width: 768px){
+ display: flex;
+ flex-direction: column;
+ @media screen and (min-width: 768px){
     padding: 0 7.5vh;
  
     }
@@ -41,12 +44,37 @@ color: white;
 
 const PaddingContianer = styled.div`
 padding: 5vh 0 0 0;
+flex-grow: 1;
 @media screen and (min-width: 768px){
     padding: 10vh 0 0 0;
 
 }
 `
+const LogosContainer = styled.div`
+display: grid;
+grid-template-columns: max-content max-content;
+width: max-content;
+margin: auto;
+ grid-gap: 0.5rem;
+ padding-bottom: 0.5rem;
 
+
+@media screen and (min-width: 768px){
+ }
+`
+;
+const LogoText = styled.div`
+font-size: 12px;
+color: black; 
+display : flex;
+ align-items: center;
+
+ font-weight: 800;
+@media screen and (min-width: 768px){
+    font-size: 18px;
+
+}
+`;
 const FullImgContent = (props) => {
     let isPageWide = media('(min-width: 768px)');
 
@@ -59,12 +87,22 @@ const FullImgContent = (props) => {
                 <span style={{marginLeft: '6px', fontWeight: '800'}}>TTWxPW</span>
             </SubText>
            {/* <SubText>Get Benefit of Exclusive Festive Offers</SubText>  */}
-
+           
             <Button  onclick={isPageWide ? ()=> window.scrollTo(0,window.innerHeight) : ()=> window.scrollTo(0,window.innerHeight*0.7)  } onclickparams={null}  fontSizeDesktop="1.25rem" link="/" margin={"1rem auto 0 auto"} marginMobile="1.5rem auto" bgColor="#f7e700" borderRadius="10px" lineHeight="1"  hoverBgColor="black" hoverColor="white" borderWidth="0px" fontWeight="600" padding="0.75rem 1.5rem">
                 {/* <BsWhatsapp style={{fontSize: '1.5rem', margin: '-0.125rem 0.25rem 0 0'}}> </BsWhatsapp> */}
                 View Trips</Button>
                 </PaddingContianer>
-               
+               {/* <Banner></Banner> */}
+               <LogosContainer>
+            <div style={{backgroundColor: 'rgba(255,255,255,0.7)', display: 'flex', padding: '0.5rem', borderRadius: '5px'}}>
+                <ImageLoader leftalign url="media/website/pwlogo.png" width="3rem" widthmobile="2rem" height="auto"></ImageLoader>
+                <LogoText className="font-opesans hden-mobile" style={{}}>Physics Wallah</LogoText>
+                </div>
+                <div style={{backgroundColor: 'rgba(255,255,255,0.7)', display: 'flex', padding: '0.5rem', borderRadius: '5px'}}>
+                <ImageLoader leftalign url="media/website/logoblack.svg" width="3rem" widthmobile="2rem"  height="auto"></ImageLoader>
+                <LogoText className="font-opesans hiden-mobile" style={{}}>The Tarzan Way</LogoText>
+                </div>
+            </LogosContainer>
         </Container>
     );
 }
