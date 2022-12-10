cdfb0a13bef0dc2ed1ee140b25e11134068a1792
Devansh Chawla <devansh@thetarzanway.com>
2022-12-10 20:31:01 +0530

    book now message bug removed

--- containers/itinerary/booking1/gittailored/Index.js
+++ containers/itinerary/booking1/gittailored/Index.js
@@ -290,7 +290,9 @@ const _startRazorpayHandler = (data) => {
     return(
     <SummaryContainer className="border-thin" style={{marginBottom: props.traveleritinerary ? '12.5vh' : '0'}}>
      {window.innerWidth > 768 ? null :  <FontAwesomeIcon icon={faTimes} onClick={props.hide} style={{textAlign: 'right'}}/>}
-    <Heading bold blur={props.blur} margin="0 auto 1.5rem auto" noline align="center">{ props.hasUserPaid ? "You're all set!" : 'Book Now'}</Heading>
+    {props.hasUserPaid ? <Heading bold blur={props.blur} margin="0 auto 1.5rem auto" noline align="center">{"You're all set!"}</Heading> :  <Heading bold blur={props.blur} margin="0 auto 1.5rem auto" noline align="center">{'Book Now'}</Heading> }
+    {/* <Heading bold blur={props.blur} margin="0 auto 1.5rem auto" noline align="center">{ props.hasUserPaid ? "You're all set!" : 'Book Now'}</Heading> */}
+
         {!oldaccommodation ? <div style={{marginBottom: '1.5rem', display: "grid", gridTemplateColumns: "1fr 1fr", gridColumnGap: "1rem"}}>
                 {props.payment.itinerary_status === ITINERARY_STATUSES.itinerary_finalized ? <p style={{fontSize: "0.75rem", fontWeight: "600", letterSpacing: "1px", marginBottom: '0.25rem'}} className={props.blur ? "font-opensans text-enter blurry-text" : "font-opensans text-enter"}>{"STARTING DATE "}</p> : <div></div>}
                 {props.payment.itinerary_status === ITINERARY_STATUSES.itinerary_finalized? <p style={{fontSize: "0.75rem", fontWeight: "600", letterSpacing: "1px", marginBottom: '0.25rem'}}  className={props.blur ? "font-opensans text-enter blurry-text" : "font-opensans text-enter"}>PAX</p>  : <div></div>}
