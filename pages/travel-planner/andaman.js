5ec9a9359572ad2b750970ba4562c630ad21da88
Devansh Chawla <devansh@thetarzanway.com>
2023-01-27 12:26:42 +0530

    andaman planner updted

--- pages/travel-planner/andaman.js
+++ pages/travel-planner/andaman.js
@@ -12,7 +12,7 @@ const DATA ={
 	"banner_text": null,
 	"overview_heading": "Andaman Overview",
 	"overview_text": "Andaman is a beautiful destination for travelers looking for a tropical paradise. It is an archipelago comprising of many islands located in the Bay of Bengal, off the eastern coast of India. Blessed with captivating beauty, it is an ideal place to relax and unwind. From pristine beaches to lush green forests, this place has something to offer to everyone. It's an ideal place to enjoy some beautiful sunsets and a great opportunity to explore the unique wildlife and marine life.\r\nThe crystal clear waters of Andaman are perfect for snorkeling and scuba diving, allowing tourists to explore the colorful coral reefs and beautiful underwater life. It offers many activities like sea walking, glass bottom boat rides, banana boat rides, and jet skiing. Andaman is also renowned for its heritage, culture, and history. It is home to many ancient monuments, caves, and forts. Tourists can also explore the tribal villages of Andaman, meet the local community, and learn about their culture and traditions.\r\nWhether you're looking for a relaxing beach holiday, a thrilling dive adventure, or an educational expedition, the Andaman Islands have something to offer for all types of travelers.",
-	"image": "https://d31aoa0ehgvjdi.cloudfront.net/media/page/167463864604858756065368652344.jpg",
+	"image": "media/page/167463864604858756065368652344.jpg",
 	"image_alt_text": "View of the Ross and Smith Island in Andaman & Nicobar Island",
 	"itineraries": [],
 	"itinerary_data": {},
