4db23b0667e46e616ddb7577713bc4c26030da57
Devansh Chawla <devansh@thetarzanway.com>
2023-03-13 14:16:40 +0530

    sentry removed

--- next.config.js
+++ next.config.js
@@ -1,25 +1,11 @@
 const withImages = require('next-images')
-const { withSentryConfig } = require('@sentry/nextjs');
+ 
 
-
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry Webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, org, project, authToken, configFile, stripPrefix,
-  //   urlPrefix, include, ignore
-
-  debug: true, // Suppresses all logs
-  silient: true,
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-};
+ 
 
 
 const moduleExports =  withImages({
-  compiler: {
-    removeConsole: true,
-  },
+ 
   async redirects() {
       return [
         {
@@ -36,4 +22,4 @@ const moduleExports =  withImages({
     },
 })
 
-module.exports = withSentryConfig(moduleExports, sentryWebpackPluginOptions);
+module.exports =  (moduleExports);
