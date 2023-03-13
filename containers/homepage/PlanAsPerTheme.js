1272fe4f4328e158b98a967dbe17c7be4bfa2d2c
Jaimin <jaimin.ttw@gmail.com>
2023-03-13 17:36:33 +0530

    homepahe college hover effect added

--- containers/homepage/PlanAsPerTheme.js
+++ containers/homepage/PlanAsPerTheme.js
@@ -1,19 +1,24 @@
+import { useRouter } from 'next/router'
 import React, { useState } from 'react'
-import styled from 'styled-components'
+import styled, { keyframes } from 'styled-components'
 import media from '../../components/media'
-import Button from '../../components/ui/button/Index'
+   import * as ga from '../../services/ga/Index';
+   import Button from '../../components/ui/button/Index'
+   
 const Container = styled.div`
-height : 300px;
+height : 320px;
   display : grid;
-  gap : 0.5rem;
+  gap : 0.2rem;
   grid-template-areas: 
-  'a a a b b b b b'
-  'a a a b b b b b'
-  'a a a b b b b b'
-  'd d e e e e e e'
+  'a a b b b b b b'
+  'a a b b b b b b'
+  'a a b b b b b b'
+  'a a b b b b b b'
+  'd e e e e e e e'
   'c c c c c c c c'
   'c c c c c c c c'
   'c c c c c c c c'
+  'c c c c c c c c';
   'c c c c c c c c';
 
   padding : 10px;
@@ -22,18 +27,18 @@ height : 300px;
     height : 550px;
     gap : 0.5rem;
     grid-template-areas: 
-    'a a a a b b b b'
-    'a a a a b b b b'
-    'a a a a b b b b'
-    'a a a a b b b b'
-    'a a a a e e e e'
-    'c c c d e e e e'
-    'c c c d e e e e';
+    'a a a a b b b b b'
+    'a a a a b b b b b'
+    'a a a a b b b b b'
+    'a a a a b b b b b'
+    'a a a a e e e e e'
+    'c c c d e e e e e'
+    'c c c d e e e e e';
 }
 
 & >.d{
-    border : 1px solid black;
-    background : white;
+    border : null;
+    background : rgba(247, 231, 0, 0.2);
     display: flex;
     flex-direction: column;
     justify-content: center;
@@ -43,17 +48,42 @@ height : 300px;
  @media screen and (min-width: 768px){
     font-size : 20px;
     text-align : center;
-    align-items : center
+    align-items : center;
+    background : white;
+    border : 1px solid black;
+
 }
 }
 
 `
 
+const TopSlideIn = keyframes`
+  from { 
+    transform: translateX(0px);
+  } 
+  to { 
+    transform: translateX(50px);
+  } 
+`;
+
+const TopSlideOut = keyframes`
+  from { 
+    transform: translateX(50px);
+  }
+  to { 
+    transform: translateX(0px);
+  } 
+`;
+
 const TextContainer = styled.div`
 position : absolute;
 top : 9px;
-left : 9px;
+right : ${props=>props.right? '9px' : null};
+left : ${props=>props.right? null : '9px'};
+text-align : ${props=>props.right? 'right' : 'left'};
 color : white;
+animation: 0.5s ${TopSlideOut};
+
 & >p{
     margin-top : -5px;
     font-weight : 500;
@@ -65,9 +95,7 @@ color : white;
 @media screen and (min-width: 768px){
     top : 30px;
     left : 30px;
-    
 }
-
 
 `
 
@@ -83,15 +111,43 @@ font-weight : 700;
 const GridItem = styled.div`
 grid-area : ${props=>props.className};
 border-radius : 8px;
-background :  url('https://d31aoa0ehgvjdi.cloudfront.net/media/website/${props=>props.bg}');
 position : relative;
+overflow :hidden;
+height : 100%;
+width : 100%;
+`
+const ImageContainer = styled.div`
+cursor : pointer;
+background-repeat: no-repeat;
+background :  url('https://d31aoa0ehgvjdi.cloudfront.net/media/website/${props=>props.bg}');
 background-repeat : round;
-
-
+height : 100%;
+width : 100%;
+.StartNow{
+    display : none;
+    top : 45px;
+    animation: 0.5s ${TopSlideIn};
+@media screen and (min-width: 768px){
+  top : 90px;
+  left : 30px;
+}
+}
+transition: 0.5s all ease-in-out ;
+&:hover{
+    transform: scale(1.1); 
+    .AnimateTop{
+        animation: 0.5s ${TopSlideIn} forwards;
+     }
+    .StartNow{
+        animation: 0.5s ${TopSlideIn} forwards;
+        display : initial;
+    }
+}
 `
 
 const PlanAsPerTheme = (props) => {
     let isPageWide = media('(min-width: 768px)')
+    const router = useRouter();
 
 
     const [loading, setLoading] = useState(false);
@@ -109,39 +165,60 @@ const PlanAsPerTheme = (props) => {
           callback: _handleTailoredRedirect,
         })
     }
+    
+    const _handleTripRedirect = (link)=>{
+      router.push(`/travel-planner/${link}-in-india`)
+    }
 
   return (
     <>
      <Container>
-        <GridItem className='a' bg='road-trip.png' >
-            <TextContainer>
+        <GridItem className='a' onClick={()=>_handleTripRedirect('road-trips')}>
+            <ImageContainer bg='road-trip.png'>
+            <TextContainer className='AnimateTop'>
                 <Heading>Road Trip</Heading>
                 <p>Planner</p>
             </TextContainer>
+            <TextContainer className='StartNow'>Start now!</TextContainer> 
+            </ImageContainer>
+
         </GridItem >
-        <GridItem className='b' bg='volunteer-travel.png'>
-        <TextContainer>
+        
+        <GridItem className='b' onClick={()=>_handleTripRedirect('volunteer')}>
+        <ImageContainer  bg='volunteer-travel.png'>
+        <TextContainer right={isPageWide?false:true} className='AnimateTop'>
                 <Heading>Volunteer Travel</Heading>
                 <p>Planner</p>
             </TextContainer>
+            <TextContainer className='StartNow'>Start now!</TextContainer> 
+
+        </ImageContainer>
         </GridItem >
-        <GridItem className='c' bg='worcation.png'>
-        <TextContainer>
+
+        <GridItem className='c' onClick={()=>_handleTripRedirect('workcation')}>
+        <ImageContainer bg='worcation.png' >
+        <TextContainer className='AnimateTop'>
                 <Heading>Workcation</Heading>
                 <p>Planner</p>
             </TextContainer>
+            <TextContainer className='StartNow'>Start now!</TextContainer> 
+
+        </ImageContainer>
         </GridItem >
         <GridItem className='d' >
         <h2 style={isPageWide?{fontSize : '50px', fontWeight : 700}:{fontSize : '18px', fontWeight : 700}}>3490</h2>
-        <p style={isPageWide?{width : '40%'}:{marginTop : '-10px'}}>Trips Planned so far.</p>
+        <p style={isPageWide?{}:{marginTop : '-10px' , marginBottom:'0px'}}>Trips Planned</p>
+        <p style={isPageWide?{marginTop : "-15px"}:{marginTop : '-5px' , marginBottom:'0px'}}>so far.</p>
         </GridItem >
-        <GridItem className='e' bg='offbeat-travel.png'>
-
-        <TextContainer>
+        <GridItem className='e' onClick={()=>_handleTripRedirect('offbeat-trips')}>
+        <ImageContainer bg='offbeat-travel.png' >
+        <TextContainer right={isPageWide?false:true} className='AnimateTop' >
                 <Heading>Offbeat</Heading>
                 <p>Planner</p>
             </TextContainer>
-        
+            <TextContainer className='StartNow'>Start now!</TextContainer> 
+
+        </ImageContainer>
         </GridItem >
     </Container>
 
