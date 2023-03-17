6d6224a0084c88a37dfd0b504d2548ca87e78857
Jaimin <jaimin.ttw@gmail.com>
2023-03-17 09:33:48 +0530

    plannerlocation skeleton added for mobile and desktop

---
+++ components/containers/plannerlocations/LocationSkeleton.js
@@ -0,0 +1,52 @@
+import React from 'react'
+import styled from 'styled-components'
+import SkeletonCard from '../../ui/SkeletonCard'
+
+const DesktopContainer = styled.div`
+display : grid;
+grid-template-columns : repeat(6,1fr);
+gap : 1rem;
+margin-inline : 8px;
+`
+
+const MobileContainer = styled.div`
+display : grid;
+grid-template-columns : 1fr 1fr;
+gap : 0.6rem;
+margin : 1rem;
+`
+
+const DesktopCard = Array(6).fill(<SkeletonCard      
+   width={"auto"}
+   height={'35vh'}
+   borderRadius={'0.5rem'}
+  lottieDimension = {'35vh'}
+   />)
+
+   const MobileCard = Array(4).fill(<SkeletonCard      
+      width={"auto"}
+      height={'35vh'}
+      borderRadius={'0.5rem'}
+     lottieDimension = {'35vh'}
+      />)
+
+const DesktopSkeleton = (props) => {
+
+    return (
+    <DesktopContainer>
+       {DesktopCard.map((e)=>e)}
+    </DesktopContainer>
+  )
+}
+
+export const MobileSkeleton = (props)=>{
+   return (
+      <MobileContainer>
+         {MobileCard.map(e=>e)}
+      </MobileContainer>
+   )
+}
+
+
+
+export default DesktopSkeleton
