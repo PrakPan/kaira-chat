9d565df76475e0ccab94bd8d34606880f406fe19
Shantanusoam007 <shantanu.ttw@gmail.com>
2023-03-20 17:51:56 +0530

    change in margin in brife route

--- containers/itinerary/breif/NewIndex.js
+++ containers/itinerary/breif/NewIndex.js
@@ -28,12 +28,12 @@ const DetailsContainer = styled.div`
   display: flex;
   gap: 10;
   flex-direction: column;
-  margin: 0 auto 10vh auto;
+  margin: 0 auto 2vh auto;
   padding: 0 1rem;
   @media screen and (min-width: 768px) {
     flex-direction: row-reverse;
     padding: 0;
-    margin: 10vh auto 2vh auto;
+    margin: 10vh auto 10vh auto;
   }
 `;
 const RouteComponent = styled.div`
