0485c09309dcbbd9bb15c7d10d5062407ff4ca98
Shantanusoam007 <shantanu.ttw@gmail.com>
2023-03-22 23:08:53 +0530

    sticky  navbar

--- containers/itinerary/New_Itenary_DBD/Day_I_Container.js
+++ containers/itinerary/New_Itenary_DBD/Day_I_Container.js
@@ -1,9 +1,9 @@
-import styled from "styled-components";
-import { useState, useEffect } from "react";
-import ItineraryFlightElement from "../../newitinerary/itineraryelements/Flight";
-import ItineraryElement from "../../newitinerary/itineraryelements/Index";
-import ItineraryFoodElement from "../../newitinerary/itineraryelements/Food";
-import ItineraryPoiElement from "../../newitinerary/itineraryelements/Poi";
+import styled from 'styled-components';
+import { useState, useEffect } from 'react';
+import ItineraryFlightElement from '../../newitinerary/itineraryelements/Flight';
+import ItineraryElement from '../../newitinerary/itineraryelements/Index';
+import ItineraryFoodElement from '../../newitinerary/itineraryelements/Food';
+import ItineraryPoiElement from '../../newitinerary/itineraryelements/Poi';
 
 const Container = styled.div`
   @media screen and (min-width: 768px) {
@@ -24,31 +24,35 @@ const Date = styled.div`
 // }
 const Day_I_Container = (props) => {
   const Arslab_elements = [
-    { name: "transfer", data: [] },
-    { name: "newcity", data: [] },
-    { name: "accommodation", data: [] },
-    { name: "meal", data: [] },
-    { name: "recommendation", data: [] },
-    { name: "activity", data: [] },
+    { name: 'transfer', data: [] },
+    { name: 'newcity', data: [] },
+    { name: 'accommodation', data: [] },
+    { name: 'meal', data: [] },
+    { name: 'recommendation', data: [] },
+    { name: 'activity', data: [] },
   ];
-  function filter(JsonArray, Arslab_element_name,Arslab_element_data) {
-    Arslab_element_data.push(JsonArray.filter(
-      (JSElement) => JSElement.element_type == Arslab_element_name
-    )[0]);
+  function filter(JsonArray, Arslab_element_name, Arslab_element_data) {
+    Arslab_element_data.push(
+      JsonArray.filter(
+        (JSElement) => JSElement.element_type == Arslab_element_name
+      )[0]
+    );
   }
   function divide(JsonArray, Arslab_elements) {
-    Arslab_elements.forEach((Arslab_element) => filter(JsonArray, Arslab_element.name,Arslab_element.data));
+    Arslab_elements.forEach((Arslab_element) =>
+      filter(JsonArray, Arslab_element.name, Arslab_element.data)
+    );
   }
   divide(props.Days.slab_elements, Arslab_elements);
-  console.log(Arslab_elements)
+  console.log(Arslab_elements);
   return (
     <Container className="font-poppins">
       <Date>Feb 3, 2023</Date>
 
-      <div className="border-thin" style={{ borderRadius: "10px" }}>
+      <div className="border-thin" style={{ borderRadius: '10px' }}>
         <ItineraryFlightElement
           time="9:00AM"
-          heading={Arslab_elements[0].data[0][0].heading}
+          heading={props.Days.slab_elements[0].heading}
           text={props.Days.slab_elements[0].text}
         ></ItineraryFlightElement>
 
@@ -63,7 +67,7 @@ const Day_I_Container = (props) => {
         ></ItineraryFoodElement>
         <ItineraryPoiElement
           time="9:00AM - 12:00PM"
-          image={"media/website/grey.png"}
+          image={'media/website/grey.png'}
           booking
           heading="Baapu Bazaar"
           text="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nullam varius aliquet viverra. Vivamus vitae felis ut nisl viverra molestie. Quisque."
--- containers/itinerary/New_Itenary_DBD/Index.js
+++ containers/itinerary/New_Itenary_DBD/Index.js
@@ -1,56 +1,59 @@
-import React from 'react'
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 import Day_I_Container from './Day_I_Container';
 import HorizontalBar from './Menubar';
 const NewItenaryDBD = (props) => {
   const Wrapper = styled.div`
-  display: flex;
-  flex-direction: column;
-  
+    display: flex;
+    flex-direction: column;
   `;
   const CitiesContainer = styled.div`
-  width: calc(100vw-32px);
-  overflow: hidden;
-  display: grid;
-  grid-template-columns: max-content max-content max-content;
-  grid-gap: 0.75rem;
-  height: max-content;
-  position: sticky;
-  top: 15vw;
-  z-index: 10;
-  background-color: white;
-`;
-const City = styled.div`
-  border-radius: 8px;
-  padding: 0.5rem;
-`;
-console.log('itenary...' + JSON.stringify(props.itinerary))
-const dates = props.itinerary.day_slabs.map((element,index)=>(
-  <div>{element.slab}</div>
-  
-))
+    width: calc(100vw-32px);
+    overflow: hidden;
+    display: grid;
+    grid-template-columns: max-content max-content max-content;
+    grid-gap: 0.75rem;
+    height: max-content;
+    position: sticky;
+    top: 31vw;
+    z-index: 10;
+    background-color: white;
+  `;
+  const City = styled.div`
+    border-radius: 8px;
+    padding: 0.5rem;
+  `;
+  console.log('itenary...' + JSON.stringify(props.itinerary));
+  const dates = props.itinerary.day_slabs.map((element, index) => (
+    <div>{element.slab}</div>
+  ));
   return (
     <Wrapper>
-           <HorizontalBar width={'100%'} height={"40px"} content={dates} ></HorizontalBar>
-        <CitiesContainer>
-        <City className='border-thin' style={{backgroundColor: 'black', color: 'white'}}>Jaipur (2N)</City>
-        <City className='border-thin' >Jodhpur (2N)</City>
-        <City className='border-thin'>Jaisalmer (2N)</City>
-        
-        </CitiesContainer>  
-        <div className='itenaryContainer'>
-        {props.itinerary.day_slabs.map((element,index)=>(
- 
-            
-          <Day_I_Container Days={element} key={element.slab_id}></Day_I_Container>
-            
-          
+      <HorizontalBar
+        width={'100%'}
+        height={'40px'}
+        content={dates}
+      ></HorizontalBar>
+      <CitiesContainer>
+        <City
+          className="border-thin"
+          style={{ backgroundColor: 'black', color: 'white' }}
+        >
+          Jaipur (2N)
+        </City>
+        <City className="border-thin">Jodhpur (2N)</City>
+        <City className="border-thin">Jaisalmer (2N)</City>
+      </CitiesContainer>
+      <div className="itenaryContainer">
+        {props.itinerary.day_slabs.map((element, index) => (
+          <Day_I_Container
+            Days={element}
+            key={element.slab_id}
+          ></Day_I_Container>
         ))}
-        </div>
-        
-        
+      </div>
     </Wrapper>
-  )
-}
+  );
+};
 
-export default NewItenaryDBD
+export default NewItenaryDBD;
