19f302caf6060532c16a77bcbe8aaac2ceee14b4
Shantanusoam007 <shantanu.ttw@gmail.com>
2023-04-05 15:45:38 +0530

    merging

--- components/containers/plannerlocations/Card.js
+++ components/containers/plannerlocations/Card.js
@@ -5,90 +5,90 @@ import media from '../../media';
 import ImageLoader from '../../ImageLoader';
 import { useRouter } from 'next/router';
 
- 
-
- 
 const ImageFade = styled.div`
-width: 100%;
-height: auto;
-transition: 0.2s all ease-in-out;
+  width: 100%;
+  height: auto;
+  transition: 0.2s all ease-in-out;
 `;
- const ImageContainer = styled.div`
-    position: relative;
-    overflow: hidden;
-    border-radius: 8px;
-    &:hover{
-        ${ImageFade}{
-          transition: 0.2s all ease-in-out;
-          transform: scale(1.1);
-         }
+const ImageContainer = styled.div`
+  position: relative;
+  overflow: hidden;
+  border-radius: 8px;
+  &:hover {
+    ${ImageFade} {
+      transition: 0.2s all ease-in-out;
+      transform: scale(1.1);
     }
-    @media screen and (min-width: 768px){
+  }
+  @media screen and (min-width: 768px) {
     height: 35vh;
-    }
+  }
+`;
 
- `;
+const BlackContainer = styled.div`
+  background-color: rgba(0, 0, 0, 0.4);
+  width: 100%;
+  height: 100%;
+  position: absolute;
+  color: white;
+  display: flex;
+  align-items: center;
+  justify-content: flex-end;
+  padding: 0.5rem;
+  top: 0;
+  flex-direction: column;
+`;
+const Heading = styled.p`
+  font-size: 1.25rem;
+  font-weight: 700;
+  line-height: 1;
+  text-align: center;
+  margin-bottom: 0.5rem;
+`;
+const Subheading = styled.p`
+  font-size: 1rem;
+  line-height: 1;
+  text-align: center;
 
+  font-weight: 300;
+`;
 
- const BlackContainer = styled.div`
- background-color: rgba(0,0,0,0.4);
- width: 100%;
- height: 100%;
- position: absolute; 
- color: white;
-display: flex;
-align-items: center;
-justify-content: flex-end;
-padding: 0.5rem;
- top: 0;
- flex-direction: column;
- 
- `;
- const Heading = styled.p`
-    font-size: 1.25rem;
-    font-weight: 700;
-    line-height: 1;
-    text-align: center;
-    margin-bottom: 0.5rem;
- `;
- const Subheading = styled.p`
- font-size: 1rem;
- line-height: 1;
- text-align: center;
+const Experiences = (props) => {
+  let isPageWide = media('(min-width: 768px)');
+  const router = useRouter();
+  /*Require props: imgWidth*/
 
-    font-weight: 300;
- `;
-
-const Experiences= (props) => {
-    let isPageWide = media('(min-width: 768px)');
-    const router = useRouter();
-    /*Require props: imgWidth*/
-  
-
-//     return(
-//       <Container onClick={props.onclick ? props.onclick : null} >  
-//           <BackroundImageLoader filter="linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.6))"   padding="0.25rem" zoomonhover center dimensions={{width: 900, height: 900}} height={isPageWide ? "40vh" : '60vh'} filters="linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))"  url={props.img}>
-//               <Name className="font-opensans">{props.heading}</Name>
-//               <Name className="font-opensans" style={{fontSize: '36px', fontWeight: '700', letterSpacing: '0'}}>{props.location}</Name>
-//           </BackroundImageLoader>
-//       </Container>
-//   ); 
-return(
-    <ImageContainer className='hover-pointer' onClick={(e) => window.location.href="https://dev.thetarzanway.com/travel-planner/"+props.link}>
-        <ImageFade><ImageLoader
-
-            url={props.img}
-            dimensions={{width: 800, height: 800}}
-            dimensionsMobile={{width: 800, height: 800}}
-            height="35vh"
-            ></ImageLoader></ImageFade>
-            <BlackContainer className='font-opensans'>
-                <Heading>{props.location}</Heading>
-                <Subheading>{props.heading}</Subheading>
-
-            </BlackContainer>
-        </ImageContainer>
-)
-}
+  //     return(
+  //       <Container onClick={props.onclick ? props.onclick : null} >
+  //           <BackroundImageLoader filter="linear-gradient(rgba(0, 0, 0, 0.2), rgba(0, 0, 0, 0.6))"   padding="0.25rem" zoomonhover center dimensions={{width: 900, height: 900}} height={isPageWide ? "40vh" : '60vh'} filters="linear-gradient(rgba(0, 0, 0, 0.9), rgba(0, 0, 0, 0.9))"  url={props.img}>
+  //               <Name className="font-opensans">{props.heading}</Name>
+  //               <Name className="font-opensans" style={{fontSize: '36px', fontWeight: '700', letterSpacing: '0'}}>{props.location}</Name>
+  //           </BackroundImageLoader>
+  //       </Container>
+  //   );
+  return (
+    <ImageContainer
+      className="hover-pointer"
+      onClick={(e) => (window.location.href = '/travel-planner/' + props.link)}
+      //   onClick={(e) =>
+      //     (window.location.href =
+      //       'https://dev.thetarzanway.com/travel-planner/' + props.link)
+      //   }
+    >
+      <ImageFade>
+        <ImageLoader
+          url={props.img}
+          dimensions={{ width: 800, height: 800 }}
+          dimensionsMobile={{ width: 800, height: 800 }}
+          height="35vh"
+        ></ImageLoader>
+      </ImageFade>
+      <BlackContainer className="font-opensans">
+        <Heading>{props.location}</Heading>
+        <Subheading>{props.heading}</Subheading>
+      </BlackContainer>
+    </ImageContainer>
+  );
+};
 
 export default Experiences;
--- containers/travelplanner/CaseStudies/Card.js
+++ containers/travelplanner/CaseStudies/Card.js
@@ -2,99 +2,137 @@ import React from 'react';
 import styled from 'styled-components';
 import Button from '../../../components/ui/button/Index';
 import ImageLoader from '../../../components/ImageLoader';
-import {ImQuotesLeft} from 'react-icons/im';
+import { ImQuotesLeft } from 'react-icons/im';
 import Flickity from './Flickity/Index';
 import { useRouter } from 'next/router';
-import {AiFillStar} from 'react-icons/ai'
- 
-const Card = styled.div`
-      padding:  0rem;
-    @media screen and (min-width: 768px){
-        padding: 2rem 0rem;
+import { AiFillStar } from 'react-icons/ai';
 
-    }
+const Card = styled.div`
+  padding: 0rem;
+  @media screen and (min-width: 768px) {
+    padding: 2rem 0rem;
+  }
 `;
 
 const CardHeading = styled.p`
-    font-size: 0.9rem;
-    font-weight: 700;
-    margin : 0;
-    margin-top : 5px;
- `;
+  font-size: 0.9rem;
+  font-weight: 700;
+  margin: 0;
+  margin-top: 5px;
+`;
 const CardSubHeading = styled.p`
-font-size: 0.9rem;
-font-weight: 400;
-margin : 0 0 0rem 0 ;
-color: rgb(122, 122, 122);
-
+  font-size: 0.9rem;
+  font-weight: 400;
+  margin: 0 0 0rem 0;
+  color: rgb(122, 122, 122);
 `;
 const CardListItem = styled.p`
-    font-size: 0.9rem;
-    font-weight: 300;
-    
-    margin : 0  ;
-    line-height: 1.5;
- `;
- const GridContainer = styled.div`
-     display: grid;
-     grid-template-columns: 1fr 1.5fr; 
-     grid-gap: 1rem;
- `;
- const ButtonContainer = styled.div`
-      &:hover{
-        background-color: black;
-     }
- `;
- const RatingContainer = styled.div`
- margin-bottom: 0.75rem;
- @media screen and (min-width: 768px){
+  font-size: 0.9rem;
+  font-weight: 300;
+
+  margin: 0;
+  line-height: 1.5;
+`;
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1.5fr;
+  grid-gap: 1rem;
+`;
+const ButtonContainer = styled.div`
+  &:hover {
+    background-color: black;
+  }
+`;
+const RatingContainer = styled.div`
+  margin-bottom: 0.75rem;
+  @media screen and (min-width: 768px) {
     flex-direction: row;
     gap: 0.75rem;
-}
- `;
+  }
+`;
 const CardContainer = (props) => {
-    const router = useRouter();
-    return (
+  const router = useRouter();
+  return (
+    <Card className="">
+      <GridContainer>
+        <div
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <ImageLoader
+            borderRadius="8px"
+            width="100%"
+            widthMobile="100%"
+            url={props.image}
+            dimensionsMobile={{ width: 600, height: 600 }}
+            dimensions={{ width: 900, height: 900 }}
+          ></ImageLoader>
+          <CardHeading className="font-opensans">{props.heading}</CardHeading>
 
-        
-            <Card className=''>
-                <GridContainer>
-                <div style={{display : 'flex' , flexDirection : 'column' , justifyContent : 'space-between' }}>
-                <ImageLoader borderRadius="8px" width="100%" widthMobile="100%"  url={props.image} dimensionsMobile={{width: 600, height: 600}} dimensions={{width: 900, height: 900}}></ImageLoader>
-                <CardHeading className='font-opensans'>{props.heading}</CardHeading>
-                
-                <RatingContainer>
-                    <div>
-                    <AiFillStar style={{color: '#FFD201', fontSize: '1.25rem', marginRight: '0.25rem'}}></AiFillStar>
-                    <AiFillStar style={{color: '#FFD201', fontSize: '1.25rem', marginRight: '0.25rem'}}></AiFillStar>
-                    <AiFillStar style={{color: '#FFD201', fontSize: '1.25rem', marginRight: '0.25rem'}}></AiFillStar>
-                    <AiFillStar style={{color: '#FFD201', fontSize: '1.25rem', marginRight: '0.25rem'}}></AiFillStar>
-                    <AiFillStar style={{color: '#FFD201', fontSize: '1.25rem'}}></AiFillStar>
+          <RatingContainer>
+            <div className="flex flex-col">
+              <AiFillStar
+                style={{
+                  color: '#FFD201',
+                  fontSize: '1.25rem',
+                  marginRight: '0.25rem',
+                }}
+              ></AiFillStar>
+              <AiFillStar
+                style={{
+                  color: '#FFD201',
+                  fontSize: '1.25rem',
+                  marginRight: '0.25rem',
+                }}
+              ></AiFillStar>
+              <AiFillStar
+                style={{
+                  color: '#FFD201',
+                  fontSize: '1.25rem',
+                  marginRight: '0.25rem',
+                }}
+              ></AiFillStar>
+              <AiFillStar
+                style={{
+                  color: '#FFD201',
+                  fontSize: '1.25rem',
+                  marginRight: '0.25rem',
+                }}
+              ></AiFillStar>
+              <AiFillStar
+                style={{ color: '#FFD201', fontSize: '1.25rem' }}
+              ></AiFillStar>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <CardSubHeading className="font-opensans">
+                {props.duration + ' • ' + props.destination}
+              </CardSubHeading>
+            </div>
+          </RatingContainer>
+          <Button
+            fontWeight="600"
+            borderRadius="6px"
+            onclick={() => router.push('/itinerary/' + props.id)}
+            fontSizeDesktop="12px"
+            borderWidth="1px"
+            width="100%"
+            bgColor="#f7e700"
+          >
+            View Details
+          </Button>
+        </div>
+        <div>
+          {/* <CardHeading className='font-opensans'>{props.heading}</CardHeading> */}
 
-                    </div>
-                    <div style={{display: 'flex', alignItems: 'center'}}>
-                    <CardSubHeading className='font-opensans'>{props.duration + " • " + props.destination}</CardSubHeading>
-
-                    </div>
-                </RatingContainer>
-                <Button fontWeight="600" borderRadius="6px" onclick={() => router.push('/itinerary/'+props.id)} fontSizeDesktop="12px" borderWidth="1px" width="100%" bgColor="#f7e700">View Details</Button>
-
-
-                </div>
-                <div>
-
-                {/* <CardHeading className='font-opensans'>{props.heading}</CardHeading> */}
-                
-                
-
-                {/* <ImQuotesLeft style={{fontSize: '1.25rem', marginLeft: '-0rem'}}></ImQuotesLeft> */}
-                <CardListItem className='font-opensans' >{props.text}</CardListItem>
-                </div>
-                </GridContainer>
-               
-            </Card>
-        
-    );
-}
+          {/* <ImQuotesLeft style={{fontSize: '1.25rem', marginLeft: '-0rem'}}></ImQuotesLeft> */}
+          <CardListItem className="font-opensans">{props.text}</CardListItem>
+        </div>
+      </GridContainer>
+    </Card>
+  );
+};
 
 export default CardContainer;
