bcf4b193105b4abf7812e11b3bd0017e538307fe
Shantanusoam007 <shantanu.ttw@gmail.com>
2023-05-04 21:05:35 +0530

    utils city page

---
+++ pages/travel-guide/[city]/things-to-do.js
@@ -0,0 +1,94 @@
+import ExperienceContainer from '../../../../containers/thingstodo/Index';
+import Layout from '../../../../components/Layout';
+import { useRouter } from 'next/router';
+import Head  from 'next/head';
+import axios from 'axios';
+import axiosallCityInstance from '../../../../services/travel-guide/SearchAllLocation'
+import axiosPoiCityInstance from '../../../../services/poi/city'
+import axiosReccommendedCityInstance from '../../../../services/poi/reccommededcities'
+
+const Experience = (props) => {
+
+    const router = useRouter();
+    return <Layout>
+         <Head>
+         <meta
+      name="description"
+      content="We envision to simplify travel and build immersive travel experiences."
+    />
+    <meta property="og:title" content={props.cityData.name +" Things To Do  | The Tarzan Way"}/><meta property="og:description" content="We envision to simplify travel and build immersive travel experiences." /><meta property="og:image" content="/logoblack.svg" />
+    <title>{props.cityData.name +" Things To Do | Travel Guide |  The Tarzan Way"}</title>
+      </Head><ExperienceContainer 
+          reccomendedCitiesData={props.reccomendedCitiesData}
+      cityData={props.cityData}   
+      id={router.query.city}></ExperienceContainer></Layout>
+// return null;
+}
+
+export async function getStaticPaths(){
+
+      // const res = await fetch(`https://apis.tarzanway.com/search/all/?type=Location`)
+      // const data = await res.json();
+
+      const res = await axiosallCityInstance.get('')
+      const data = res.data
+
+      let paths = [];
+      for(var i = 0 ; i<data.length ; i++){
+            if(data[i].cta){
+                  paths.push({
+                        params: {
+                              city: data[i].cta
+                        }
+                  })
+            }
+      }
+      // let paths = data.map((object) => {
+      //       if(object.cta !==null)
+
+      //             return {
+      //                   params: {
+      //                         city: object.cta
+      //                   }
+      //             }
+      // })
+      return{
+            fallback: true, 
+            paths: paths,
+            fallback: 'blocking'
+
+             
+      }
+}
+export async function getStaticProps(context){
+
+      // const res = await fetch(`https://apis.tarzanway.com/poi/city/?slug=`+context.params.city)
+      // const data = await res.json()
+
+      const res = await axiosPoiCityInstance.get(`/?slug=${context.params.city}`)
+      const data = res.data
+
+      try{
+            const resp = await axiosReccommendedCityInstance.get(
+              `/?slug=${context.params.city}`
+            );
+            const reccoData = resp.data;
+       var reccomendedCitiesData = reccoData.map(e=>({id : e.id , image : e.image , lat : e.lat , long : e.long , most_popular_for : e.most_popular_for , name : e.name})) 
+          }
+          catch{
+        var reccomendedCitiesData = null
+          }
+      if (!data) {
+            return {
+              notFound: true,
+            }
+          }
+      return{
+            props: {
+                  cityData: data,
+                  reccomendedCitiesData
+            }
+      }
+}
+
+export default Experience
