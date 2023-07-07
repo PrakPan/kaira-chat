3c9c3e10db8a2fdc2ade67cb1ffb528e20f87c32
jaimin <jaimin.ttw@gmail.com>
2023-07-07 09:46:52 +0530

    chatbot name and email register issue resolved

--- components/Layout.js
+++ components/Layout.js
@@ -28,6 +28,37 @@ const Layout = React.memo((props) => {
   //   }
   // }, [router.isReady, router.asPath]);
 
+  // Freshchat bot :-
+
+
+  useEffect(() => {
+      var name;
+      if (localStorage.getItem("name"))
+        name = localStorage.getItem("name").split(" ");
+      var email = localStorage.getItem("email");
+
+    function handleWidgetLoaded() {
+      if (name?.length && email) {
+        window.fcWidget.user.setFirstName(name[0]);
+        window.fcWidget.user.setEmail(email);
+      }
+    }
+    if (window.fcWidget) {
+      window.fcWidget.on("widget:loaded", handleWidgetLoaded());
+    } else {
+      setTimeout(() => {
+        if (window.fcWidget) {
+          window.fcWidget.on("widget:loaded", handleWidgetLoaded());
+        }
+      }, 5000);
+    }
+    return () => {
+      if (window.fcWidget) {
+        window.fcWidget.off("widget:loaded", handleWidgetLoaded());
+      }
+    };
+  }, [props.token]);
+
   return (
     <div className="layout">
       <Header
