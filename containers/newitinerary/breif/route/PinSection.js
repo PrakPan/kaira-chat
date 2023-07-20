1c8723b496ee2c1da4df1edd4d20a8bdb5801219
jaimin <jaimin.ttw@gmail.com>
2023-07-20 16:56:52 +0530

    Itinerary mapbox zoom and center fixed

--- containers/newitinerary/breif/route/PinSection.js
+++ containers/newitinerary/breif/route/PinSection.js
@@ -1,22 +1,53 @@
-import styled from 'styled-components';
-import React, { useState, useEffect } from 'react';
-import { useHover } from '../../../../hooks/useHover';
-import Pin from './Pin';
+import styled from "styled-components";
+import React, { useState, useEffect } from "react";
+import { useHover } from "../../../../hooks/useHover";
+import Pin from "./Pin";
+import { MdNavigateNext } from "react-icons/md";
 
 const Container = styled.div`
   cursor: pointer;
   display: grid;
   grid-template-columns: max-content auto max-content;
+  // &:hover > .heading::after {
+  //   width: 100%;
+  // }
+  &:hover {
+    .IconContainer {
+      right: -30px;
+    }
+  }
 `;
 const Heading = styled.div`
+  width: fit-content;
   font-weight: 500;
-
   color: black;
-
   font-size: 20px;
   line-height: 28px;
   display: flex;
   align-items: center;
+  position: relative;
+
+  // ----
+  // position: relative;
+  // display: inline-block;
+
+  // &::after {
+  //   content: "";
+  //   position: absolute;
+  //   left: 0;
+  //   bottom: -5px; /* Adjust this value to control the underline thickness */
+  //   width: 0;
+  //   height: 2px;
+  //   background-color: ${({ pinColour }) => pinColour};
+  //   transform-origin: left;
+  //   transition: width 0.2s ease; /* Smooth transition on width change */
+  // }
+`;
+const IconContainer = styled.div`
+  position: absolute;
+  right: -23px;
+  opacity: 0.55;
+  transition: right 0.2s ease;
 `;
 
 const PinSection = ({
@@ -68,11 +99,15 @@ const PinSection = ({
     setShowDrawer(true);
     setShowDrawerData(cityData);
   };
-  console.log('duration;: ', duration);
   return (
     <Container className="cursor-pointer " onClick={() => handleClick()}>
       <Pin duration={duration} pinColour={pinColour}></Pin>
-      <Heading className={`${setCurrentPopup ? 'ml-4' : 'lg:ml-8 ml-2'} `}>
+      <Heading
+        pinColour={pinColour}
+        className={`${
+          setCurrentPopup ? "ml-4 heading" : "lg:ml-8 ml-2 heading"
+        } `}
+      >
         {duration >= 1
           ? city +
             `${
@@ -84,6 +119,12 @@ const PinSection = ({
         {/* <div className="px-4 py-1 text-[12px] cursor-pointer border-2 border-black ml-6 font-bold font-lexend text-black rounded-md">
           Edit
         </div> */}
+        <IconContainer className="IconContainer">
+          <MdNavigateNext
+            style={{ fontSize: "1.5rem" }}
+            className="AnimateRight"
+          />
+        </IconContainer>
       </Heading>
     </Container>
   );
