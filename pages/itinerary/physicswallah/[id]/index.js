1ac8500e0749f14f6415f56c5fcff112e3acc148
jaimin <jaimin.ttw@gmail.com>
2023-09-18 17:57:02 +0530

    PW itinerary static nav done

--- pages/itinerary/physicswallah/[id]/index.js
+++ pages/itinerary/physicswallah/[id]/index.js
@@ -22,18 +22,24 @@ const Itinerary = (props) => {
 
 }, []);
      return (
-    <Layout itinerary  hidecta PW>
-      {/* <script src='//in.fw-cdn.com/30401267/225580.js' chat='true'></script> */}
-        <Head>
-            <title>Physics Wallah Itinerary | The Tarzan Way</title>
-            <meta property="og:title" content="Physics Wallah Itinerary | The Tarzan Way" />
-            <meta property="og:description" content="We envision to simplify travel and build immersive travel experiences." />
-            <meta property="og:image" content="/logoblack.svg" />
-            {/* <script
+       <Layout itinerary staticnav hidecta PW>
+         {/* <script src='//in.fw-cdn.com/30401267/225580.js' chat='true'></script> */}
+         <Head>
+           <title>Physics Wallah Itinerary | The Tarzan Way</title>
+           <meta
+             property="og:title"
+             content="Physics Wallah Itinerary | The Tarzan Way"
+           />
+           <meta
+             property="og:description"
+             content="We envision to simplify travel and build immersive travel experiences."
+           />
+           <meta property="og:image" content="/logoblack.svg" />
+           {/* <script
               src='https://thetarzanway-web.s3.us-west-2.amazonaws.com/scripts/itinerarybot.js'
          
           /> */}
-             {/* <script
+           {/* <script
               
               dangerouslySetInnerHTML={{
                 __html: `
@@ -42,12 +48,16 @@ const Itinerary = (props) => {
             `,
               }}
             /> */}
-        </Head>
-        {/* <script dangerouslySetInnerHTML={{
+         </Head>
+         {/* <script dangerouslySetInnerHTML={{
     __html: `(function (d, w, c) { if(!d.getElementById("spd-busns-spt")) { var n = d.getElementsByTagName('script')[0], s = d.createElement('script'); var loaded = false; s.id = "spd-busns-spt"; s.async = "async"; s.setAttribute("data-self-init", "false"); s.setAttribute("data-init-type", "opt"); s.src = 'https://cdn.in-freshbots.ai/assets/share/js/freshbots.min.js'; s.setAttribute("data-client", "3225c221f3048e75e5a6ef1d6a5227c59290c8f1"); s.setAttribute("data-bot-hash", "74b6cd8cbe305eba5699361061f2c6fc1ec8607b"); s.setAttribute("data-env", "prod"); s.setAttribute("data-region", "in"); if (c) { s.onreadystatechange = s.onload = function () { if (!loaded) { c(); } loaded = true; }; } n.parentNode.insertBefore(s, n); } }) (document, window, function () { Freshbots.initiateWidget({ autoInitChat: false, getClientParams: function () { return {"cstmr::eml":"","cstmr::phn":"","cstmr::nm":""}; } }, function(successResponse) { }, function(errorResponse) { }); });`}}/> */}
 
-        <ItineraryContainer  key={router.asPath}  id={router.query.id}></ItineraryContainer>
-    </Layout>);
+         <ItineraryContainer
+           key={router.asPath}
+           id={router.query.id}
+         ></ItineraryContainer>
+       </Layout>
+     );
 }
 
 const mapStateToPros = (state) => {
