2fef6be058bfe53935279136bd833f8d0172a804
Bhat Owais <owais.ttw@gmail.com>
2024-04-12 16:56:49 +0530

    Updated date input

--- containers/newitinerary/breif/route/RouteEditSection.js
+++ containers/newitinerary/breif/route/RouteEditSection.js
@@ -1,8 +1,7 @@
-import React, { useRef } from "react";
+import React, { useRef, useState, useEffect } from "react";
 import { connect } from "react-redux";
 import { openNotification } from "../../../../store/actions/notification";
 import useMediaQuery from "../../../../components/media";
-import { useState, useEffect } from "react";
 import { IoMenu, IoLocationSharp } from "react-icons/io5";
 import { FaLocationCrosshairs } from "react-icons/fa6";
 import { BiSolidPencil } from "react-icons/bi";
@@ -26,8 +25,137 @@ import {
 import axiossearchstartinginstance from "../../../../services/search/startinglocation";
 import axiossearchinstance from "../../../../services/search/searchsuggest";
 import axiosItineraryUpdateInstance from "../../../../services/itinerary/update";
-import { convertDateFormat } from "../../../../helper/ConvertDateFormat";
 import { dateFormat } from "../../../../helper/DateUtils";
+import "react-dates/initialize";
+import "react-dates/lib/css/_datepicker.css";
+import { BiCalendarAlt } from "react-icons/bi";
+import moment from "moment";
+import media from "../../../../components/media";
+import { DateRangePicker, SingleDatePicker } from "react-dates";
+import styled from "styled-components";
+
+const Container = styled.div`
+  position: relative;
+
+  .DateRangePickerInput {
+  display: flex;
+  flex-direction: column;
+}
+  .DateRangePicker {
+    width: 100%;
+  }
+  .DateRangePickerInput_1 {
+    border: none;
+    display: flex;
+    gap: 22px;
+    background: initial;
+  }
+  .DateInput {
+    width: 100%;
+    border: 1px solid #d0d5dd;
+    box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+    border-radius: 8px;
+    overflow: hidden;
+  }
+  .DateInput > input {
+    font-family: poppins;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+  .DayPicker__withBorder {
+    @media screen and (max-width: 768px) {
+      border: none;
+      -webkit-box-shadow: none;
+      box-shadow: none;
+      width: 320px;
+      margin: auto;
+    }
+  }
+  .DateRangePickerInput_arrow,
+  .DayPickerKeyboardShortcuts_buttonReset {
+    display: none !important;
+  }
+
+  .DateRangePicker_picker_1 {
+    left: 0px;
+    top: 48px !important;
+    @media screen and (min-width: 768px) {
+      left: 0px !important;
+      right: 0px !important;
+      top: 55px !important;
+  }
+  .CalendarDay {
+    border: 0px;
+  }
+  .CalendarDay__selected,
+  .CalendarDay__selected:hover {
+    background-color: #f7e700;
+    border: 0px;
+    color: black;
+  }
+  .CalendarDay__selected_span,
+  .CalendarDay__hovered_span,
+  .CalendarDay__hovered_span_3 {
+    background-color: #f7e70033;
+    color: black;
+    &:active {
+      background-color: #f7e700;
+      opacity: 0.7;
+      border: none;
+    }
+    &:hover {
+      color: black;
+      background-color: #f7e7004a;
+      border: none;
+    }
+  }
+
+  .DateInput_input__focused {
+    border-bottom: 2px solid #f7e700;
+  }
+  .DayPickerKeyboardShortcuts_show__topRight {
+    display: none;
+  }
+`;
+
+const CalenderIcons = styled.div`
+  position: absolute;
+  top: 0%;
+  right: 0%;
+  pointer-events: none;
+  font-size: 20px;
+  z-index: 0;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  width: 100% !important;
+  height: 7.5rem;
+`;
+
+const TextContainer = styled.div`
+  display: flex;
+  justify-content: space-between;
+  font-weight: 400;
+  font-size: 14px;
+  height: 25px;
+  width: 100%;
+`;
+
+const Text = styled.p`
+  width: 100%;
+`;
+
+const Icon = styled.div`
+  width: 100%;
+  text-align: right;
+  width: 100%;
+  text-align: right;
+  display: flex;
+  align-items: center;
+  justify-content: end;
+  margin-right: 5px;
+  margin-top: -5px;
+`;
 
 const RouteEditSection = (props) => {
   const isDesktop = useMediaQuery("(min-width:768px)");
@@ -950,50 +1078,55 @@ export const DestinationDates = (props) => {
   // }, [checkinDate, checkoutDate]);
 
   const handleDateChange = (e) => {
-    if (e.target.name === "Arrival Date") {
-      const offSet = differenceInDays(
-        new Date(e.target.value),
-        new Date(checkinDate)
-      );
-      if (isNaN(Date.parse(checkinDate))) {
-        setCheckinDate(e.target.value);
-      } else {
-        handleDates(offSet, index, e.target.value, checkoutDate, true);
-      }
-    } else if (e.target.name === "Departure Date") {
-      const offSet = differenceInDays(
-        new Date(e.target.value),
-        new Date(checkoutDate)
-      );
-      if (isNaN(Date.parse(checkoutDate))) {
-        setCheckoutDate(e.target.value);
-      } else {
-        handleDates(offSet, index, checkinDate, e.target.value);
-      }
-    } else if (e.target.name === "Start Date") {
-      const offSet = differenceInDays(
-        new Date(e.target.value),
-        new Date(startDate)
-      );
-      if (isNaN(Date.parse(startDate))) {
-        setStartDate(e.target.value);
-      } else {
-        handleDates(offSet, index, null, null);
-        setStartDate(e.target.value);
-      }
-    } else if (e.target.name === "End Date") {
-      setEndDate(e.target.value);
+    console.log(e);
+    if (e) {
+      if (e.target.name === "Arrival Date") {
+        const offSet = differenceInDays(
+          new Date(e.target.value),
+          new Date(checkinDate)
+        );
+        if (isNaN(Date.parse(checkinDate))) {
+          setCheckinDate(e.target.value);
+        } else {
+          handleDates(offSet, index, e.target.value, checkoutDate, true);
+        }
+      } else if (e.target.name === "Departure Date") {
+        const offSet = differenceInDays(
+          new Date(e.target.value),
+          new Date(checkoutDate)
+        );
+        if (isNaN(Date.parse(checkoutDate))) {
+          setCheckoutDate(e.target.value);
+        } else {
+          handleDates(offSet, index, checkinDate, e.target.value);
+        }
+      } else if (e.target.name === "Start Date") {
+        const offSet = differenceInDays(
+          new Date(e.target.value),
+          new Date(startDate)
+        );
+        if (isNaN(Date.parse(startDate))) {
+          setStartDate(e.target.value);
+        } else {
+          handleDates(offSet, index, null, null);
+          setStartDate(e.target.value);
+        }
+      } else if (e.target.name === "End Date") {
+        setEndDate(e.target.value);
 
-      // const offSet = differenceInDays(
-      //   new Date(e.target.value),
-      //   new Date(endDate)
-      // );
+        // const offSet = differenceInDays(
+        //   new Date(e.target.value),
+        //   new Date(endDate)
+        // );
 
-      // if (isNaN(Date.parse(endDate))) {
-      //   setEndDate(e.target.value);
-      // } else {
-      //   handleDates(offSet, index, null, null);
-      // }
+        // if (isNaN(Date.parse(endDate))) {
+        //   setEndDate(e.target.value);
+        // } else {
+        //   handleDates(offSet, index, null, null);
+        // }
+      }
+    } else {
+      console.log("here >>>", date);
     }
   };
 
@@ -1155,14 +1288,32 @@ export const DestinationDates = (props) => {
         <div ref={cityRef} className="w-full flex flex-col gap-2 py-3">
           <div className="w-full flex flex-row items-center gap-3">
             <div className="w-full flex flex-col gap-1">
-              <div>
+              <label>
                 {startingCity
                   ? "Start Date"
                   : endingCity
                   ? "End Date"
                   : "Arrival Date"}
-              </div>
-              <div className="w-full flex flex-col items-start gap-1">
+              </label>
+              <DatePicker
+                date={
+                  startingCity
+                    ? startDate
+                    : endingCity
+                    ? endDate
+                    : getDate(cityData.checkin_date)
+                }
+                onDateChange={handleDateChange}
+                id={
+                  startingCity
+                    ? "Start Date"
+                    : endingCity
+                    ? "End Date"
+                    : "Arrival Date"
+                }
+              />
+
+              {/* <div className="flex flex-row items-center gap-1">
                 <input
                   required
                   name={
@@ -1220,14 +1371,19 @@ export const DestinationDates = (props) => {
                       }
                     </div>
                   )}
-              </div>
+              </div> */}
             </div>
           </div>
           {!(startingCity || endingCity) && (
             <div className="flex flex-row items-center gap-3">
               <div className="flex flex-col gap-1">
-                <div>Departure Date</div>
-                <div className="flex flex-col items-start gap-1">
+                <label htmlFor="endDate">Departure Date</label>
+                <DatePicker
+                  date={getDate(cityData.checkout_date)}
+                  onDateChange={handleDateChange}
+                  id={"Departure Date"}
+                />
+                {/* <div className="flex flex-row items-center gap-1">
                   <input
                     required
                     name={"Departure Date"}
@@ -1249,7 +1405,7 @@ export const DestinationDates = (props) => {
                       {isInvalidDate(checkoutDate, true).message}
                     </div>
                   )}
-                </div>
+                </div> */}
               </div>
             </div>
           )}
@@ -1391,6 +1547,81 @@ export const Month = ({ firstDay, days, startDate, endDate }) => {
           );
         })}
       </div>
+    </div>
+  );
+};
+
+export const DatePicker = (props) => {
+  const [focusedInput, setFocusedInput] = useState(props.focusedDate || null);
+
+  useEffect(() => {
+    if (props.setFocusedDate) {
+      if (focusedInput) props.setFocusedDate(focusedInput);
+      else props.setFocusedDate(undefined);
+    }
+  }, [focusedInput]);
+
+  useEffect(() => {
+    if (props.setFocusedDate) setFocusedInput(props.inputDate);
+  }, [props.inputDate]);
+
+  let isPageWide = media("(min-width: 768px)");
+
+  return (
+    <div className="">
+      <Container className="flex flex-col">
+        {/* <DateRangePicker
+          readOnly={true}
+          displayFormat="DD/MM/YYYY"
+          startDate={props.valueStart}
+          startDateId="startDate"
+          startDatePlaceholderText="DD/MM/YYYY"
+          startDateTitleText="Start Date"
+          endDate={props.valueEnd}
+          endDateId="endDate"
+          endDatePlaceholderText="DD/MM/YYYY"
+          endDateTitleText="End Date"
+          onDatesChange={({ startDate, endDate }) => {
+            console.log("end >>>>", endDate?._d);
+            props.setValueStart(startDate?._d);
+            props.setValueStart(endDate?._d);
+          }}
+          focusedInput={focusedInput}
+          onFocusChange={setFocusedInput}
+          isOutsideRange={(day) =>
+            day.startOf("day").isBefore(moment().add(-1, "day"))
+          }
+          initialVisibleMonth={() => moment().subtract(0, "month")}
+          numberOfMonths={1}
+          orientation={"horizontal"}
+          noBorder={true}
+        /> */}
+        <SingleDatePicker
+          date={moment(props.date)} // momentPropTypes.momentObj or null
+          onDateChange={(date) =>
+            props.onDateChange({
+              target: {
+                name: props.id,
+                value: date,
+              },
+            })
+          } // PropTypes.func.isRequired
+          focused={false} // PropTypes.bool
+          onFocusChange={({ focused }) => focused} // PropTypes.func.isRequired
+          id={props.id} // PropTypes.string.isRequired,
+          noBorder={true}
+          placeholder={"DD/MM/YYYY"}
+          numberOfMonths={1}
+        />
+        <CalenderIcons className="p-2 py-3">
+          <Icon>
+            <BiCalendarAlt />
+          </Icon>
+          {/* <Icon>
+            <BiCalendarAlt />
+          </Icon> */}
+        </CalenderIcons>
+      </Container>
     </div>
   );
 };
