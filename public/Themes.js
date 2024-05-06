6c97faf1342912bfbfb76084694c2ceedb814681
Bhat Owais <owais.ttw@gmail.com>
2024-05-06 18:25:50 +0530

    Updated .gitignore

--- public/Themes.js
+++ public/Themes.js
@@ -1,13 +1,20 @@
-
-/* ORIGINAL THEME
+// VIJAY'S THEME
 
 const theme = {
-    colors: {
-      brandColor: "#F7e700"
-    },
-    fontsizes: {
-      desktop: {
-        headings: {
+  font: {
+    nunito: "Nunito",
+    opensans: "Open Sans",
+  },
+  colors: {
+    brandColor: "#F7E700",
+    secondary: "#000000",
+    tartiary: "#ffffff",
+    error: "#F70000",
+    success: "#05F700",
+  },
+  fontsizes: {
+    desktop: {
+      headings: {
         one: "5rem",
         two: "4rem",
         three: "3rem",
@@ -22,75 +29,21 @@ const theme = {
         five: "0.85rem",
         six: "0.75rem",
         seven: "0.5rem",
-      }
-      },
-      mobile: {
-        headings: {
-          one: "2.5rem",
-          two: "1.75rem",
-          three: "1.5rem",
-        },
-        text: {
-          default: "1rem",
-          one: "1.5rem",
-          two: "1.25rem",
-          three: "1rem",
-          four: "0.85rem",
-          five:"0.5rem",
-        }
-      }
-    } 
-  };
-
-export default theme;
-*/
-
-// VIJAY'S THEME
-
-const theme = {
-  font: {
-    nunito: 'Nunito',
-    opensans: 'Open Sans',
-  },
-  colors: {
-    brandColor: '#F7E700',
-    secondary: '#000000',
-    tartiary: '#ffffff',
-    error: '#F70000',
-    success: '#05F700',
-  },
-  fontsizes: {
-    desktop: {
-      headings: {
-        one: '5rem',
-        two: '4rem',
-        three: '3rem',
-        four: '2.5rem',
-      },
-      text: {
-        default: '1.1rem',
-        one: '2rem',
-        two: '1.5rem',
-        three: '1.25rem',
-        four: '1rem',
-        five: '0.85rem',
-        six: '0.75rem',
-        seven: '0.5rem',
       },
     },
     mobile: {
       headings: {
-        one: '2.5rem',
-        two: '2rem',
-        three: '1.25rem',
+        one: "2.5rem",
+        two: "2rem",
+        three: "1.25rem",
       },
       text: {
-        default: '1rem',
-        one: '1.5rem',
-        two: '1.25rem',
-        three: '1rem',
-        four: '0.85rem',
-        five: '0.6rem',
+        default: "1rem",
+        one: "1.5rem",
+        two: "1.25rem",
+        three: "1rem",
+        four: "0.85rem",
+        five: "0.6rem",
       },
     },
   },
