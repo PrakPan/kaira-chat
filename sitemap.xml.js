5c50dd89520fdf8a3cc4eaf573b05b29de2f5439
Bhat Owais <owais.ttw@gmail.com>
2024-06-24 13:58:19 +0530

    Added sitemap.xml

---
+++ sitemap.xml.js
@@ -0,0 +1,94 @@
+// const { default: axios } = require("axios");
+const axios = require("axios");
+const fs = require("fs");
+const path = require("path");
+
+const generateSitemap = async () => {
+  const BASE_URL = "https://thetarzanway.com";
+
+  // Fetch continents list
+  const continents = await axios.get(
+    "https://apis.tarzanway.com/page/list?page_type=Continents&fields=path"
+  );
+  const continentsData = continents.data;
+  let continentsPaths = continentsData.map((object) => {
+    return { title: "Continent Planner", link: BASE_URL + "/" + object.path };
+  });
+
+  // Fetch countries list
+  const countries = await axios.get(
+    "https://apis.tarzanway.com/poi/country/all?fields=path"
+  );
+  const countriesData = countries.data;
+  let countriesPaths = countriesData.map((object) => {
+    return { title: "Country Planner", link: BASE_URL + "/" + object.path };
+  });
+
+  // Fetch states list
+  const states = await axios.get(
+    "https://apis.tarzanway.com/search/all/?type=State&fields=path"
+  );
+  const statesData = states.data;
+  let statesPaths = statesData.map((object) => {
+    return {
+      title: "State Planner",
+      link: BASE_URL + "/" + object.path,
+    };
+  });
+
+  // Fetch cities list
+  const cities = await axios.get(
+    "https://apis.tarzanway.com/search/all/?type=Location&fields=path"
+  );
+  const citiesData = cities.data;
+
+  let cityPaths = citiesData.map((object) => {
+    return { title: "City Planner", link: BASE_URL + "/" + object.path };
+  });
+
+  const StaticPaths = [
+    { title: "Home Page", link: BASE_URL },
+    { title: "Travel Guide", link: BASE_URL + "/travel-guide" },
+    {
+      title: "COVID-19 Safe Travel India",
+      link: BASE_URL + "/covid-19-safe-travel-india",
+    },
+    { title: "Travel Experiences", link: BASE_URL + "/travel-experiences" },
+  ];
+
+  const allPaths = [
+    ...StaticPaths,
+    ...continentsPaths,
+    ...countriesPaths,
+    ...statesPaths,
+    ...cityPaths,
+  ];
+
+  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${allPaths
+        .map((el) => {
+          return `
+            <url>
+              <loc>${el.link}</loc>
+              <lastmod>${new Date().toISOString()}</lastmod>
+              <changefreq>monthly</changefreq>
+              <priority>1.0</priority>
+            </url>
+          `;
+        })
+        .join("")}
+    </urlset>
+  `;
+
+  fs.writeFileSync(
+    path.join(process.cwd(), "public", "sitemap.xml"),
+    sitemap,
+    "utf8"
+  );
+  console.log("Sitemap generated successfully!");
+};
+
+generateSitemap().catch((error) => {
+  console.error("Error generating sitemap:", error);
+});
