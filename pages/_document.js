45825f0a9f8c456c54812fe39f60406f50b6a14b
Bhat Owais <owais.ttw@gmail.com>
2024-06-24 17:25:28 +0530

    Updated _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -32,13 +32,13 @@ export default class MyDocument extends Document {
           {/* Google Tag Manager */}
           {process.env.NODE_ENV === "production" &&
             !CONTENT_SERVER_HOST.includes("dev") && (
-              <scrip
+              <script
                 dangerouslySetInnerHTML={{
                   __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-                })(window,document,'script','dataLayer','${GOOGLE_ANALTICS_ID}');`,
+                    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                    })(window,document,'script','dataLayer','${GOOGLE_ANALTICS_ID}');`,
                 }}
               />
             )}
@@ -56,13 +56,13 @@ export default class MyDocument extends Document {
               <script
                 dangerouslySetInnerHTML={{
                   __html: `
-                window.dataLayer = window.dataLayer || [];
-                function gtag(){dataLayer.push(arguments);}
-                gtag('js', new Date());
-                gtag('config', '${GOOGLE_ANALTICS_ID}', {
-                page_path: window.location.pathname,
-                });
-                `,
+                    window.dataLayer = window.dataLayer || [];
+                    function gtag(){dataLayer.push(arguments);}
+                    gtag('js', new Date());
+                    gtag('config', '${GOOGLE_ANALTICS_ID}', {
+                    page_path: window.location.pathname,
+                    });
+                    `,
                 }}
               />
             )}
@@ -88,7 +88,7 @@ export default class MyDocument extends Document {
                     c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
                     t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
                     y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-                })(window, document, "clarity", "script", "dxk3hzpt0s");`,
+                    })(window, document, "clarity", "script", "dxk3hzpt0s");`,
                 }}
               />
             )}
