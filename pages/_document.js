841f594900ee7146e4587d67487cd3eab07c36b5
Bhat Owais <owais.ttw@gmail.com>
2024-06-24 17:29:41 +0530

    Updated _document.js

--- pages/_document.js
+++ pages/_document.js
@@ -25,7 +25,7 @@ export default class MyDocument extends Document {
           {/* Google Tag Manager */}
           {process.env.NODE_ENV === "production" &&
             !CONTENT_SERVER_HOST.includes("dev") && (
-              <scrip
+              <script
                 dangerouslySetInnerHTML={{
                   __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
                 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
