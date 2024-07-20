718c2152097c9456cc70f4f7fc26b36cfa117560
Bhat Owais <owais.ttw@gmail.com>
2024-07-20 07:58:23 +0530

    Updated user profile edit

--- containers/userprofile/EditProfile.js
+++ containers/userprofile/EditProfile.js
@@ -1,58 +1,451 @@
-import { useState } from "react";
-import { MdDone } from "react-icons/md";
+import Image from "next/image";
+import { useEffect, useState } from "react";
 import { connect } from "react-redux";
+import { MdDone } from "react-icons/md";
+import styled from "styled-components";
+import OTPInput from "react-otp-input";
+import { BiError } from "react-icons/bi";
+import { FiChevronDown } from "react-icons/fi";
+import CountryCodeDropdown from "../../components/userauth/CountryDropdown";
+import { userEmailEditInstance } from "../../services/user/edit";
+import axiosuserinstance from "../../services/user/edit";
 import * as authaction from "../../store/actions/auth";
+import extensions from "../../public/content/extensionsdata";
 
-const mapDispatchToProps = (dispatch) => {
+const CountryCodeContainer = styled.div`
+  position: relative;
+  width: 90px;
+  height: 3.1rem;
+  .CountryInput {
+    display: grid;
+    border: 2px solid #d0d5dd;
+    border-radius: 0.5rem;
+    grid-template-columns: 1fr 1fr 1fr;
+    padding-inline: 0.2rem;
+    gap: 0.4rem;
+    height: 100%;
+    paddding-left: 10%;
+  }
+  img {
+    margin-block: auto;
+  }
+  p {
+    margin: auto;
+  }
+  svg {
+    margin-block: auto;
+    font-size: 1.3rem;
+    margin-left: -5px;
+  }
+`;
+
+const CountryImg = styled(Image)`
+  height: 1.5rem;
+  alt: "";
+`;
+
+const CountryCodeOption = styled.div`
+  display: grid;
+  grid-template-columns: 0.7fr max-content;
+  padding-inline: 0.2rem;
+  gap: 0.6rem;
+  &:hover {
+    cursor: pointer;
+  }
+  text-align: center;
+  height: 2rem !important;
+  margin-block: 0.5rem;
+
+  p {
+    margin: auto;
+  }
+`;
+
+const OtpContainer = styled.div`
+  div {
+    width: 60%;
+    display: grid !important;
+    grid-template-columns: 1fr 1fr 1fr 1fr !important;
+    gap: 0.8rem;
+  }
+  .otpBox {
+    width: 100% !important;
+    border: 1px solid #d0d5dd;
+    border-radius: 8px;
+    height: 3rem;
+    box-shadow: 0px 1px 2px rgba(16, 24, 40, 0.05);
+  }
+`;
+
+const ErrorText = styled.div`
+  color: red;
+  font-size: 13px;
+  margin-top: 5px;
+  margin-left: 5px;
+  height: 1rem;
+  display: flex;
+  align-items: center;
+`;
+
+const mapStateToProps = (state) => {
   return {
-    changeUserDetails: (payload) =>
-      dispatch(authaction.changeUserDetails(payload)),
+    token: state.auth.token,
   };
 };
 
-const EditInput = connect(
-  null,
-  mapDispatchToProps
-)(({ type, name, text, closeEdit, changeUserDetails }) => {
+const mapDispatchToProps = (dispatch) => {
+  return {
+    setUserDetails: (payload) => dispatch(authaction.setUserDetails(payload)),
+  };
+};
+
+export const EditInput = connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(({ token, type, name, text, closeEdit, setUserDetails }) => {
   const [value, setValue] = useState(text);
   const [loading, setLoading] = useState(false);
+  const [optSent, setOptSent] = useState(false);
+  const [extension, setExtension] = useState("India");
+  const [openCountryCodeOption, setOpenCountryCodeOption] = useState(false);
+  const [ExtensionOptions, setExtensionOptions] = useState([]);
+
+  useEffect(() => {
+    let Options = [];
+    for (const country in extensions) {
+      Options.push(
+        <CountryCodeOption
+          key={country}
+          value={country}
+          onClick={() => {
+            handleExtensionChangeOption(country),
+              setOpenCountryCodeOption(false);
+          }}
+        >
+          <CountryImg
+            height="29"
+            width="29"
+            objectFit="cover"
+            src={extensions[country].img}
+            onClick={() => handleExtensionChangeOption(country)}
+          ></CountryImg>
+          <p>{extensions[country].label}</p>
+        </CountryCodeOption>,
+      );
+    }
+
+    setExtensionOptions(Options);
+  }, []);
+
+  useEffect(() => {
+    if (name === "phone") {
+      const { countryCode, number } = separateCountryCode(text);
+      if (countryCode && number) {
+        setValue(number);
+        const country = getCountryName(countryCode);
+        if (country) {
+          setExtension(country);
+        } else {
+          setExtension("India");
+        }
+      }
+    }
+  }, []);
+
+  const separateCountryCode = (phoneNumber) => {
+    const pattern = /^(\+\d{1,3})(\d{10})$/;
+    const match = phoneNumber.match(pattern);
+
+    if (match) {
+      const countryCode = match[1];
+      const number = match[2];
+
+      return {
+        countryCode: countryCode,
+        number: number,
+      };
+    } else {
+      return null; // Invalid phone number format
+    }
+  };
+
+  const getCountryName = (code) => {
+    for (const country in extensions) {
+      if (extensions[country].label === code) {
+        return extensions[country].value;
+      }
+    }
+    return null;
+  };
 
   const handleSave = () => {
-    if (value !== text) {
-      setLoading(true);
-      let data = {};
-      data[name] = value;
-      changeUserDetails(data);
-      setLoading(false);
+    if (token) {
+      if (
+        (name === "phone" && extensions[extension].label + value !== text) ||
+        value != text
+      ) {
+        setLoading(true);
+        let data = {};
+        data[name] = value;
+
+        switch (name) {
+          case "phone":
+            handlePhone({
+              data: { phone: extensions[extension].label + value },
+            });
+            break;
+          case "email":
+            handleEmail({ email: value });
+            break;
+          default:
+            handleName({ data });
+            break;
+        }
+      } else {
+        closeEdit(false);
+      }
+    } else {
+      closeEdit(false);
     }
-    closeEdit(false);
+  };
+
+  const handleName = ({ data }) => {
+    axiosuserinstance
+      .patch("", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setUserDetails(res.data);
+        setLoading(false);
+        closeEdit(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        closeEdit(false);
+        if (err.response.data.name) {
+          console.log(err.response.data.name[0]);
+        } else {
+          console.log(err.response.data);
+        }
+      });
+  };
+
+  const handlePhone = ({ data }) => {
+    setOptSent(false);
+    axiosuserinstance
+      .post("phone_change/initiate/", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setLoading(false);
+        setOptSent(true);
+      })
+      .catch((err) => {
+        setLoading(false);
+        closeEdit(false);
+        if (err.response.data.phone) {
+          console.log(err.response.data.phone[0]);
+        } else {
+          console.log(err.response.data);
+        }
+      });
+  };
+
+  const handleEmail = ({ email }) => {
+    setOptSent(false);
+    userEmailEditInstance
+      .get(`initiate/?email=${email}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setLoading(false);
+        setOptSent(true);
+      })
+      .catch((err) => {
+        setLoading(false);
+        closeEdit(false);
+        if (err.response.data.email) {
+          console.log(err.response.data.email[0]);
+        } else {
+          console.log(err.response.data);
+        }
+      });
+  };
+
+  const handleExtensionChangeOption = (country) => {
+    setExtension(country);
   };
 
   return (
-    <div
-      className={`w-full flex flex-row items-center gap-3 ${
-        name === "name" && "justify-center"
-      }`}
-    >
-      <input
-        autoFocus
-        disabled={loading}
-        name={name}
-        type={type}
-        value={value}
-        // onBlur={() => closeEdit(false)}
-        onChange={(e) => setValue(e.target.value)}
-        className={`w-[60%] border-2 border-gray-200 rounded-md p-2 focus:outline-none ${
-          loading && "opacity-25"
+    <div className="w-full flex flex-col gap-2">
+      <div
+        className={`w-full flex flex-row items-center gap-3 ${
+          name === "name" && "justify-center"
         }`}
-      ></input>
-      {loading ? (
-        <div className="w-6 h-6 rounded-full animate-spin border-t-2 border-black"></div>
-      ) : (
-        <MdDone onClick={handleSave} className="text-2xl cursor-pointer" />
+      >
+        {name === "phone" && (
+          <CountryCodeContainer>
+            <div
+              className="CountryInput"
+              onClick={() => setOpenCountryCodeOption(true)}
+            >
+              <CountryImg
+                height="29"
+                width="29"
+                objectFit="cover"
+                src={extensions[extension].img}
+              ></CountryImg>
+
+              <p>{extensions[extension].label} </p>
+              <FiChevronDown />
+            </div>
+            {openCountryCodeOption && (
+              <CountryCodeDropdown
+                onClose={() => setOpenCountryCodeOption(false)}
+                ExtensionOptions={ExtensionOptions}
+              />
+            )}
+          </CountryCodeContainer>
+        )}
+
+        <input
+          autoFocus
+          disabled={loading}
+          name={name}
+          type={type}
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+          className={`w-[60%] border-2 border-[#d0d5dd] rounded-md px-2 py-[0.64rem] focus:outline-none ${
+            loading && "opacity-25"
+          }`}
+        ></input>
+
+        {loading ? (
+          <div className="w-6 h-6 rounded-full animate-spin border-t-2 border-black"></div>
+        ) : optSent ? (
+          <button
+            onClick={handleSave}
+            className="text-sm text-blue cursor-pointer underline"
+          >
+            Resend OTP
+          </button>
+        ) : (
+          <MdDone onClick={handleSave} className="text-2xl cursor-pointer" />
+        )}
+      </div>
+
+      {optSent && (
+        <div className="flex flex-col gap-2">
+          <div className="text-gray-500">OTP has been sent</div>
+
+          <OPTInput
+            name={name}
+            token={token}
+            phone={extensions[extension].label + value}
+            email={value}
+            setUserDetails={setUserDetails}
+            closeEdit={closeEdit}
+          />
+        </div>
       )}
     </div>
   );
 });
 
-export { EditInput };
+const OPTInput = ({ name, token, phone, email, setUserDetails, closeEdit }) => {
+  const [value, setValue] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if (value.length === 4) {
+      switch (name) {
+        case "phone":
+          handlePhoneOPT({ data: { phone, otp: value } });
+          break;
+        case "email":
+          handleEmailOPT({ otp: value });
+          break;
+        default:
+          return;
+      }
+    }
+  }, [value]);
+
+  const handlePhoneOPT = ({ data }) => {
+    setLoading(true);
+    axiosuserinstance
+      .patch("phone_change/complete/", data, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setUserDetails(res.data);
+        setLoading(false);
+        closeEdit(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err.response.data.otp) {
+          console.log(err.response.data.otp[0]);
+          setError(err.response.data.otp[0]);
+        } else {
+          console.log(err.response.data);
+        }
+      });
+  };
+
+  const handleEmailOPT = ({ otp }) => {
+    setLoading(true);
+    userEmailEditInstance
+      .get(`complete/?otp=${otp}`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      })
+      .then((res) => {
+        setUserDetails(res.data);
+        setLoading(false);
+        closeEdit(false);
+      })
+      .catch((err) => {
+        setLoading(false);
+        if (err.response.data.otp) {
+          console.log(err.response.data.otp[0]);
+          setError(err.response.data.otp[0]);
+        } else {
+          console.log(err.response.data);
+        }
+      });
+  };
+
+  return (
+    <div className="flex flex-col gap-2">
+      <OtpContainer className={`${loading && "opacity-25"}`}>
+        <OTPInput
+          value={value}
+          onChange={(otp) => setValue(otp)}
+          numInputs={4}
+          inputType="tel"
+          inputStyle="otpBox"
+          renderInput={(props) => <input {...props} />}
+        />
+      </OtpContainer>
+      {error && (
+        <ErrorText>
+          <BiError style={{ fontSize: "1rem" }} />
+          <span style={{ marginLeft: "2px", marginTop: "2px" }}>
+            OTP is not valid
+          </span>
+        </ErrorText>
+      )}
+    </div>
+  );
+};
--- services/user/edit.js
+++ services/user/edit.js
@@ -1,8 +1,12 @@
-import axios from 'axios';
-import {CONTENT_SERVER_HOST} from '../constants';
+import axios from "axios";
+import { CONTENT_SERVER_HOST } from "../constants";
 
 const instance = axios.create({
-    baseURL: CONTENT_SERVER_HOST+"/user/info"
-})
+  baseURL: CONTENT_SERVER_HOST + "/user/info/",
+});
 
 export default instance;
+
+export const userEmailEditInstance = axios.create({
+  baseURL: CONTENT_SERVER_HOST + "/user/verify_email/",
+});
