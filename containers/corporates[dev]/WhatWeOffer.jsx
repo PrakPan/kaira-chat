54e57236f31674bb4a9e3697566ad6d7c750f8d9
Bhat Owais <owais.ttw@gmail.com>
2024-10-22 11:32:46 +0530

    Updated font on corporates pages

--- containers/corporates[dev]/WhatWeOffer.jsx
+++ containers/corporates[dev]/WhatWeOffer.jsx
@@ -1,39 +1,23 @@
 import { GoArrowRight } from "react-icons/go";
 import Button from "../../components/ui/button/Index"
 import ImageLoader from "../../components/ImageLoader";
-import openTailoredModal from "../../services/openTailoredModal";
-import { logEvent } from "../../services/ga/Index";
-import { useRouter } from "next/router";
 import media from "../../components/media";
 
 
 export default function WhatWeOffer(props) {
-    const router = useRouter();
     let isPageWide = media("(min-width: 768px)");
 
-    const handleCreateTripButton = () => {
-        openTailoredModal(router);
-
-        logEvent({
-            action: "Plan_Itinerary",
-            params: {
-                page: "Corporates Page",
-                event_category: "Button Click",
-                event_label: "Plan Itinerary Now!",
-                event_action: "What We Offer",
-            },
-        });
-    };
-
     return (
-        <div className="mt-5 py-5 flex flex-col md:flex-row">
+        <div className="mt-5 py-5 flex flex-col gap-2 md:flex-row">
             <div className="px-2 md:px-0">
                 <ImageLoader url={"media/corporates/whatweoffer.jpeg"} width={isPageWide ? 580 : 290} height={isPageWide ? 600 : 300} borderRadius={8} />
             </div>
-            <div className="pt-3 pb-3 pl-3 pr-3 md:py-5 md:px-16 flex flex-col gap-4">
+            <div className="pl-3 pr-3 md:pt-0 md:px-16 flex flex-col justify-between">
                 <div className="text-[27px] md:text-[40px] font-[700] leading-[52px]">What We Offer?</div>
-                <div className="flex flex-col gap-3 text-[16px] font-[400] leading-[28px]">
-                    <div className="">Explore the world with curated travel experiences that cater to every adventurer. We offer personalized itineraries, unique accommodations, and immersive activities that go beyond the typical tourist spots. Whether you're seeking a tranquil retreat, an adrenaline-pumping adventure, or a cultural deep dive, our expertly crafted trips ensure you experience the best of each destination. Let us handle the details while you focus on creating unforgettable memories.</div>
+                <div className="flex flex-col gap-3 text-[16px] font-[350] leading-[28px]">
+                    <div className="">
+                        Explore the world with curated travel experiences that cater to every adventurer. We offer personalized itineraries, unique accommodations, and immersive activities that go beyond the typical tourist spots. Whether you're seeking a tranquil retreat, an adrenaline-pumping adventure, or a cultural deep dive, our expertly crafted trips ensure you experience the best of each destination. Let us handle the details while you focus on creating unforgettable memories.
+                    </div>
                     <div className="flex flex-col gap-3">
                         <div className="flex flex-row items-center gap-2">
                             <GoArrowRight className="text-3xl" />
