65044264eb57e1b6ed8554deb34b1092b15a2114
Bhat Owais <owais.ttw@gmail.com>
2024-11-04 15:23:54 +0530

    updated dynamic filters in hotel search

---
+++ components/modals/bookingupdated/filtersmobile/UserRatings.jsx
@@ -0,0 +1,36 @@
+import { IoMdStar } from "react-icons/io";
+
+export default function UserRatings(props) {
+    const handleUserStar = (star) => {
+        if (props.selectedUserStar.includes(star)) {
+            props.setSelectedUserStar(prev => prev.filter(item => item !== star));
+        } else {
+            props.setSelectedUserStar(prev => [...prev, star])
+        }
+    }
+
+    const isSelectedUserStar = (star) => {
+        return props.selectedUserStar.includes(star);
+    }
+
+    return (
+        <div className="flex flex-col justify-start items-baseline">
+            <div className="mb-2 font-normal">User Ratings</div>
+            <div className="flex flex-row gap-1">
+                {props.userRatings.map((star, i) => (
+                    <button
+                        key={i}
+                        onClick={() => handleUserStar(star)}
+                        className={`flex font-normal  text-sm cursor-pointer  justify-center items-center hover:bg-gray-100 active:bg-[#111] active:border-0 ${isSelectedUserStar(star)
+                            ? "text-white border-0 bg-black "
+                            : "border-2 bg-white text-black"
+                            } active:text-white  border-[#D0D5DD]  rounded-lg px-2 py-1`}
+                    >
+                        {star}
+                        <IoMdStar />
+                    </button>
+                ))}
+            </div>
+        </div>
+    );
+}
