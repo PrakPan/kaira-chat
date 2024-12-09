956a95277da0c1d3fc64e994b61c5f956f2f0138
Bhat Owais <owais.ttw@gmail.com>
2024-12-09 17:17:25 +0530

    Added desktop banner on new year page

--- containers/newYear/index.jsx
+++ containers/newYear/index.jsx
@@ -1,6 +1,7 @@
 import { useRouter } from "next/router";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Banner from "../homepage/banner/Mobile";
+import DesktopBanner from "../../components/containers/Banner";
 import media from "../../components/media";
 import openTailoredModal from "../../services/openTailoredModal";
 import NewYearBanner from "../../components/newYear/banner/banner";
@@ -11,49 +12,54 @@ import WhyUs from "../../components/newYear/WhyUs";
 import OurCustomers from "../../components/newYear/OurCustomers";
 import BannerCards from "../../components/newYear/BannerCards";
 
-
 const NewYearPage = (props) => {
-    const router = useRouter();
-    let isPageWide = media("(min-width: 768px)");
+  const router = useRouter();
+  let isPageWide = media("(min-width: 768px)");
 
-    return (
-        <div className="space-y-[50px] md:space-y-[100px] mb-[100px]">
-            <NewYearBanner
-                heightmobile={"30rem"}
-                height={"40rem"}
-                filter={"brightness(0.8)"}
-                zIndex={-1}
-                center={isPageWide ? false : true}
-                url={"media/new-year/new-year.jpeg"}
-            >
-                <BannerContent />
-            </NewYearBanner>
+  return (
+    <div className="space-y-[50px] md:space-y-[100px] mb-[100px]">
+      <NewYearBanner
+        heightmobile={"30rem"}
+        height={"40rem"}
+        filter={"brightness(0.8)"}
+        zIndex={-1}
+        center={isPageWide ? false : true}
+        url={"media/new-year/new-year.jpeg"}
+      >
+        <BannerContent />
+      </NewYearBanner>
 
-            <div className="w-[85%] flex flex-col mx-auto gap-[50px] md:gap-[100px]">
-                <Packages />
+      <DesktopBanner
+        newYear
+        onclick={() => openTailoredModal(router)}
+        text="Want to personalize your own experience?"
+      ></DesktopBanner>
 
-                <NewYearUnique />
+      <div className="w-[85%] flex flex-col mx-auto gap-[50px] md:gap-[100px]">
+        <Packages />
 
-                <WhyUs />
+        <NewYearUnique />
 
-                <BannerCards />
+        <WhyUs />
 
-                <OurCustomers />
-            </div>
+        <BannerCards />
 
-            {!isPageWide && (
-                <div>
-                    <Banner
-                        onclick={() => openTailoredModal(router)}
-                        text="Want to craft your own travel experience?"
-                        buttontext="Start Now"
-                        color="black"
-                        buttonbgcolor="#f7e700"
-                    ></Banner>
-                </div>
-            )}
+        <OurCustomers />
+      </div>
+
+      {!isPageWide && (
+        <div>
+          <Banner
+            onclick={() => openTailoredModal(router)}
+            text="Want to craft your own travel experience?"
+            buttontext="Start Now"
+            color="black"
+            buttonbgcolor="#f7e700"
+          ></Banner>
         </div>
-    );
+      )}
+    </div>
+  );
 };
 
 export default NewYearPage;
