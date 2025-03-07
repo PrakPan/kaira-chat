fbfe34e2c5b3a318322feff8a1c7af6318182d40
PrakharX-bit <prakhar.ttw@gmail.com>
2025-03-07 17:59:43 +0530

    City Detail Fixes

--- containers/newitinerary/breif/route/PinSection.js
+++ containers/newitinerary/breif/route/PinSection.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import React from "react";
 import Pin from "./Pin";
 import { MdNavigateNext } from "react-icons/md";
+import { axiosCityDataById } from "../../../../services/itinerary/brief/route";
 
 const Container = styled.div`
   cursor: pointer;
@@ -32,11 +33,25 @@ const IconContainer = styled.div`
   transition: right 0.2s ease;
 `;
 
+
 const PinSection = (props) => {
-  const handleClick = () => {
+  
+
+  const handleClick = async () => {
+    if(!props?.mercury){
     if (!props.duration || props.duration === "0" || props.transfersPin) return;
     props.setShowDrawer(true);
     props.setShowDrawerData(props.cityData);
+    }else{
+      if (!props.duration || props.duration === "0" || props.transfersPin) return;
+      if(props?.cityId){
+      const res = await axiosCityDataById.get(`/${props?.cityId}`);
+      const data = res.data;
+      console.log("City Data",data);
+      props.setShowDrawer(true);
+    props.setShowDrawerData(data.city);
+      }
+    }
   };
   
 
--- services/itinerary/brief/route.js
+++ services/itinerary/brief/route.js
@@ -1,8 +1,12 @@
 import axios from 'axios';
-import { MIS_SERVER_HOST } from '../../constants';
+import { MERCURY_HOST, MIS_SERVER_HOST } from '../../constants';
 
 const routes = axios.create({
   baseURL: MIS_SERVER_HOST + '/sales/itinerary/routes',
+});
+
+export const axiosCityDataById = axios.create({
+  baseURL: MERCURY_HOST + '/api/v1/geos/city',
 });
 
 export default routes;
