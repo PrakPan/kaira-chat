f62b34fddf279966d78a1155a7e3574781fe6e6c
Bhat Owais <owais.ttw@gmail.com>
2025-04-07 10:46:03 +0530

    Updated login modal

--- store/reducers/CountryCodes.js
+++ store/reducers/CountryCodes.js
@@ -1,7 +1,23 @@
 import * as actionTypes from "../actions/actionsTypes";
 
 // Define initial state
-const initialState = null;
+const initialState = {
+  India: {
+    img: "https://flagcdn.com/in.svg",
+    label: "+91",
+    value: "India",
+  },
+  "United Kingdom": {
+    img: "https://flagcdn.com/gb.svg",
+    label: "+44",
+    value: "United Kingdom",
+  },
+  "United States": {
+    img: "https://flagcdn.com/us.svg",
+    label: "+1",
+    value: "United States",
+  },
+};
 
 // Define reducer
 const reducer = (state = initialState, action) => {
