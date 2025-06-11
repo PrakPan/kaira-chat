512341430e1c9d98cee7a9b0bfb3260aae5944a8
PrakharX-bit <prakhar@thetarzanway.com>
2025-06-11 14:00:04 +0530

    Bug fixes

--- components/modals/accommodation/roomtypes/roomtype/Index.js
+++ components/modals/accommodation/roomtypes/roomtype/Index.js
@@ -40,7 +40,15 @@ const RoomType = (props) => {
           <div className="text-md md:text-lg font-bold">
             Room {props.index + 1}
           </div>
-          {props.rooms.length > 1 && (
+          {(props.rooms.length > 1 || 
+            props.rooms.some(room => 
+              room?.description || 
+              room?.images?.length > 0 || 
+              room?.facilities?.length > 0
+            ) ||
+            (props.data?.polices && props.data.polices.length > 0) ||
+            props.data?.cancellation_policies
+          ) && (
             <div className="text-blue">
               {open ? (
                 <div className="flex flex-row items-center gap-1 hover:bg-black hover:text-white p-1 rounded-lg cursor-pointer">
@@ -61,24 +69,6 @@ const RoomType = (props) => {
           <div className="text-xl md:text-2xl font-bold">
             {"₹" + getIndianPrice(Math.round(props.data?.final_rate))}
           </div>
-
-          {/* <div className="flex flex-col gap-1 items-end">
-            <Button
-              onclick={props.handleUpdateBooking(props.index)}
-              bgColor={"#F7E700"}
-              borderRadius="8px"
-              fontWeight="400"
-              hoverColor="white"
-              height={"full"}
-              padding={"8px 16px"}
-            >
-              Add To Itinerary
-            </Button>
-
-            <div className="text-sm">
-              on {props.checkInDate} ({props.city})
-            </div>
-          </div> */}
         </div>
       </div>
 
@@ -126,7 +116,13 @@ const RoomType = (props) => {
                 </div>
               ) : null}
 
-              {props.rooms.length === 1 && (
+              {(props.rooms.length === 1 && (
+                room?.description || 
+                room?.images?.length > 0 || 
+                room?.facilities?.length > 0 ||
+                (props.data?.polices && props.data.polices.length > 0) ||
+                props.data?.cancellation_policies
+              )) && (
                 <div className="text-blue">
                   {open ? (
                     <div className="w-fit flex flex-row items-center gap-1 hover:bg-black hover:text-white p-1 rounded-lg cursor-pointer">
@@ -146,57 +142,52 @@ const RoomType = (props) => {
 
           {open && (
             <div className="flex flex-col gap-3">
-              {props.rooms.map((room, index) => (
-                <div key={index} className="flex flex-col gap-3">
-                  <div
-                    key={index}
-                    className={`flex flex-col md:flex-row gap-1 ${
-                      room?.description && room?.images?.length > 0
-                        ? "justify-center"
-                        : room?.images?.length > 0
-                        ? "justify-end"
-                        : ""
-                    }`}
-                  >
-                    <div className="flex justify-start w-full">
-                      {room?.description ? (
-                        <div
-                          dangerouslySetInnerHTML={{
-                            __html: room.description,
-                          }}
-                          className=""
-                        ></div>
-                      ) : null}
-                    </div>
-                    <div className="flex justify-end w-full">
-                      {room?.images?.length > 0 && (
-                        <div className="flex flex-col items-center justify-center gap-3 md:w-[40%] h-[250px]">
-                          <ImageCarousel images={room?.images} />
-                        </div>
-                      )}
-                    </div>
-                  </div>
-
-                  {room?.facilities?.length > 0 ? (
-                    <div className="flex flex-col gap-2">
-                      <div className="text-lg font-semibold">
-                        Room Amenities
-                      </div>
-                      <div className="text-[14px]">
-                        <div className="flex flex-wrap gap-2">
-                          {room.facilities.map((item, index) => (
-                            <div key={index}>
-                              <div className="bg-[#FAFAFA] p-[8px] rounded-[10px]">
-                                {item}
-                              </div>
-                            </div>
-                          ))}
-                        </div>
-                      </div>
-                    </div>
+              <div
+                className={`flex flex-col md:flex-row gap-1 ${
+                  room?.description && room?.images?.length > 0
+                    ? "justify-center"
+                    : room?.images?.length > 0
+                    ? "justify-end"
+                    : ""
+                }`}
+              >
+                <div className="flex justify-start w-full">
+                  {room?.description ? (
+                    <div
+                      dangerouslySetInnerHTML={{
+                        __html: room.description,
+                      }}
+                      className=""
+                    ></div>
                   ) : null}
                 </div>
-              ))}
+                <div className="flex justify-end w-full">
+                  {room?.images?.length > 0 && (
+                    <div className="flex flex-col items-center justify-center gap-3 md:w-[40%] h-[250px]">
+                      <ImageCarousel images={room?.images} />
+                    </div>
+                  )}
+                </div>
+              </div>
+
+              {room?.facilities?.length > 0 ? (
+                <div className="flex flex-col gap-2">
+                  <div className="text-lg font-semibold">
+                    Room Amenities
+                  </div>
+                  <div className="text-[14px]">
+                    <div className="flex flex-wrap gap-2">
+                      {room.facilities.map((item, facilityIndex) => (
+                        <div key={facilityIndex}>
+                          <div className="bg-[#FAFAFA] p-[8px] rounded-[10px]">
+                            {item}
+                          </div>
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                </div>
+              ) : null}
             </div>
           )}
         </div>
@@ -204,9 +195,9 @@ const RoomType = (props) => {
 
       {open && (
         <div className="flex flex-col gap-3">
-          {props.data?.polices && props?.data?.polices?.length>0
+          {props.data?.polices && props?.data?.polices?.length > 0
             ? props.data.polices.map((item, index) => (
-                <div className="flex flex-col gap-2">
+                <div key={index} className="flex flex-col gap-2">
                   <div className="text-lg font-semibold">{item.type}</div>
                   <div
                     className="text-[14px]"
@@ -219,13 +210,13 @@ const RoomType = (props) => {
             : null}
         </div>
       )}
+      
       <div
-                    className="text-[14px]"
-                    dangerouslySetInnerHTML={{
-                      __html: props?.data?.cancellation_policies,
-                    }}
-                  ></div>
-      {/* <CancellationPolicy policies={props?.data?.cancellation_policies}/> */}
+        className="text-[14px]"
+        dangerouslySetInnerHTML={{
+          __html: props?.data?.cancellation_policies,
+        }}
+      ></div>
     </div>
   );
 };
