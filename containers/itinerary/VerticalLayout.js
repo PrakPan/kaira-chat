d0e95d995a10c17c2a66f83cad3fea545b8d78fd
PrakharX-bit <prakhar@thetarzanway.com>
2025-07-22 19:00:05 +0530

    Pickup drop fixes

--- containers/itinerary/VerticalLayout.js
+++ containers/itinerary/VerticalLayout.js
@@ -980,10 +980,10 @@ const CityItem = ({
           transferData.transferType === "pickup"
             ? dCityData?.id || dCityData?.gmaps_place_id
             : oCityData?.id || oCityData?.gmaps_place_id,
-        destination_itinerary_city:
-          transferData.transferType === "pickup"
-            ? dCityData?.id || dCityData?.gmaps_place_id
-            : oCityData?.id || oCityData?.gmaps_place_id,
+        destination_itinerary_city: null,
+          // transferData.transferType === "pickup"
+          //   ? dCityData?.id || dCityData?.gmaps_place_id
+          //   : oCityData?.id || oCityData?.gmaps_place_id,
         is_pickup: transferData.transferType === "pickup",
         is_drop: transferData.transferType === "drop",
         source: transferData?.source,
