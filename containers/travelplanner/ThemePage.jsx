6c5e7804fa801b6b8b6ca23efd4b7b2382f4ccea
Prakhar Pandey <prakhar@theatarzanway.com>
2025-08-11 10:45:35 +0530

    Theme Page fixes

--- containers/travelplanner/ThemePage.jsx
+++ containers/travelplanner/ThemePage.jsx
@@ -137,7 +137,7 @@ export default function ThemePage(props) {
         />
       )}
 
-      {props?.slug === "ladakh" || props?.slug === "thailand" ? (
+      {props?.slug === "ladakh" ? (
         <>
           <HeroBannerLadakh
             image={props.experienceData.image}
