2970d235c330e1a3bcfe8ffdd3ee870d05cc88c7
Karanveer Sharma <karanveer@thetarzanway.com>
2025-08-21 12:31:05 +0530

    added link to create a trip

--- components/v2/home/HeadingContent.jsx
+++ components/v2/home/HeadingContent.jsx
@@ -8,6 +8,7 @@ import {
   useGSAP,
 } from "../common/gsapConfig";
 import styles from "./HeadingContent.module.scss";
+import Link from "next/link";
 
 const HeadingContent = ({ title, subtitle }) => {
   const headingRef = useRef(null);
@@ -74,7 +75,10 @@ const HeadingContent = ({ title, subtitle }) => {
         <span className="absolute inset-0 overflow-hidden rounded-full">
           <span className="absolute inset-0 rounded-full bg-[image:radial-gradient(75%_100%_at_50%_0%,rgba(251, 247, 19, 0.6)_0%,rgba(56,189,248,0)_75%)] opacity-0 transition-opacity duration-500 group-hover:opacity-100" />
         </span>
-        <div className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2 px-6 ring-1 ring-white/10">
+        <Link
+          href="/new-trip"
+          className="relative flex space-x-2 items-center z-10 rounded-full bg-zinc-950 py-2 px-6 ring-1 ring-white/10 text-white"
+        >
           <span>Create a Trip</span>
           <svg
             fill="none"
@@ -91,7 +95,7 @@ const HeadingContent = ({ title, subtitle }) => {
               strokeWidth="1.5"
             />
           </svg>
-        </div>
+        </Link>
         <span className="absolute -bottom-0 left-[1.125rem] h-px w-[calc(100%-2.25rem)] bg-gradient-to-r from-emerald-400/0 via-emerald-400/90 to-emerald-400/0 transition-opacity duration-500 group-hover:opacity-40" />
       </button>
     </div>
