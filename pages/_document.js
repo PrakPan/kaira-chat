64cc67f79e5e0f7fd70929e2e1257fba001142cf
Karanveer Sharma <karansharma10293847@gmail.com>
2025-09-23 15:04:45 +0530

    added title

--- pages/_document.js
+++ pages/_document.js
@@ -31,6 +31,7 @@ export default class MyDocument extends Document {
     return (
       <Html id="html" lang="en">
         <Head>
+        <title>Trip Planner & Itinerary | The Tarzan Way</title>
           <meta
             name="viewport"
             content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=5"
