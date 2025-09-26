fe3c49b4ab505d2ca4c6a54e44b4953887b5c883
Prakhar Pandey <prakhar@theatarzanway.com>
2025-09-26 10:11:22 +0530

    Fix:: Components Path

--- pages/_app.js
+++ pages/_app.js
@@ -18,7 +18,8 @@ import restartBot from "../helper/RestartBot";
 import { useDispatch } from "react-redux";
 import { authLogout } from "../store/actions/auth";
 import { cleanExpiredLocalStorage } from "../services/localStorageUtils";
-import JupiterAnalytics from "../components/jupyterAnalytics";
+import JupiterAnalytics from "../components/JupiterAnalytics";
+
 
 function MyApp({ Component, pageProps, store }) {
   const router = useRouter();
