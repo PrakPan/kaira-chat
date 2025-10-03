b6acceb0e6233972ba08e26aa7fad246a3beca09
Prakhar Pandey <prakhar@theatarzanway.com>
2025-10-03 14:17:29 +0530

    Fix:: Edit Route 0 Duration Checks

--- containers/newitinerary/breif/route/PinSection.js
+++ containers/newitinerary/breif/route/PinSection.js
@@ -91,7 +91,7 @@ const PinSection = (props) => {
             `${
               props.duration > 1
                 ? ` - ${props.duration} Nights`
-                : `${props.duration == 0 ? `- 0 Night` : ` - ${props.duration}`}  Night`
+                : `${props.duration == 0 ? `- 0 ` : ` - ${props.duration}`}  Night`
             } `
           : props.city}
         {props.transfersPin || props.duration == null ? (
