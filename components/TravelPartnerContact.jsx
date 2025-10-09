08d87c2832d264355f7e869aecd84250e1f32ade
Karanveer Sharma <karansharma10293847@gmail.com>
2025-10-09 12:44:16 +0530

    made setting ui

--- components/TravelPartnerContact.jsx
+++ components/TravelPartnerContact.jsx
@@ -2,33 +2,81 @@ import Image from "next/image";
 import { SocialShareDesktop } from "../containers/itinerary/booking1/SocialShare";
 import { useState } from "react";
 import useMediaQuery from "../hooks/useMedia";
+import ModalWithBackdrop from "./ui/ModalWithBackdrop";
+import Settings from "./settings/Index";
+import BottomModal from "./ui/LowerModal";
 
 export default function TravelPartnerContact(props) {
-    const [share, setShare] = useState(false);
-    const isDesktop = useMediaQuery("(min-width:767px)");
-
-    return (
-        <div className="flex items-center gap-3">
-            <div className="flex items-center gap-2">
-            <button className="flex items-center justify-center w-9 h-9 rounded-full bg-gray-100 hover:bg-gray-200">
-                    <Image src={"/settings.svg"} height={22} width={22} className=" text-gray-600" />
-                </button>
-                {isDesktop&&<div className="relative">
-                <button className="flex items-center justify-center w-9 h-9 rounded-full bg-gray-100 hover:bg-gray-200" onClick={()=>setShare(true)}>
-                    <Image src="/share.svg" height={22} width={22} className="text-gray-600" />
-                </button>
-                {share&&<div className="absolute top-10 right-0 z-[99]">
-                    <SocialShareDesktop
-                    social_title={props?.itinerary?.social_title}
-                    social_description={props?.social_description}
-                    itineraryName={props?.itinerary?.name}
-                    itineraryImage={props?.itinerary?.images?.[0]}
-                    share={share}
-                    setShare={setShare}
-                    />
-                </div>}
-                </div>}
-            </div>
-        </div>
-    );
+  const [share, setShare] = useState(false);
+  const isDesktop = useMediaQuery("(min-width:767px)");
+  const [showSettings, setShowSettings] = useState(false);
+  return (
+    <div className="flex items-center gap-3">
+      <div className="flex items-center gap-2">
+        <button
+          className="flex items-center justify-center w-9 h-9 rounded-full bg-gray-100 hover:bg-gray-200"
+          onClick={() => setShowSettings(true)}
+        >
+          <Image
+            src={"/settings.svg"}
+            height={22}
+            width={22}
+            className=" text-gray-600"
+          />
+        </button>
+        {isDesktop && (
+          <div className="relative">
+            <button
+              className="flex items-center justify-center w-9 h-9 rounded-full bg-gray-100 hover:bg-gray-200"
+              onClick={() => setShare(true)}
+            >
+              <Image
+                src="/share.svg"
+                height={22}
+                width={22}
+                className="text-gray-600"
+              />
+            </button>
+            {share && (
+              <div className="absolute top-10 right-0 z-[99]">
+                <SocialShareDesktop
+                  social_title={props?.itinerary?.social_title}
+                  social_description={props?.social_description}
+                  itineraryName={props?.itinerary?.name}
+                  itineraryImage={props?.itinerary?.images?.[0]}
+                  share={share}
+                  setShare={setShare}
+                />
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+      {isDesktop ?<ModalWithBackdrop
+        centered
+        show={showSettings == true}
+        mobileWidth="100%"
+        backdrop
+        closeIcon={true}
+        onHide={() => setShowSettings(false)}
+        borderRadius={"12px"}
+        animation={false}
+        backdropStyle={{
+          backgroundColor: "rgba(0,0,0,0.4)",
+          backdropFilter: "blur(1px)",
+        }} // <- add this
+      >
+        <Settings />
+      </ModalWithBackdrop>:
+      <BottomModal
+        show={showSettings == true}
+        onHide={() => setShowSettings(false)}
+        width="100%"
+        height="max-content"
+      >
+        <Settings />
+      </BottomModal>
+      }
+    </div>
+  );
 }
---
+++ components/settings/DateComponent.jsx
@@ -0,0 +1,107 @@
+import { useState } from "react";
+import { StyledFigmaBox } from "../tailoredform/utils/ui";
+import { getHumanDate } from "../../services/getHumanDate";
+import Image from "next/image";
+import { useSelector } from "react-redux";
+import ModalWithBackdrop from "../ui/ModalWithBackdrop";
+import AirbnbCalendar from "../calendar";
+import BottomModal from "../ui/LowerModal";
+import AirbnbCalendarMobile from "../calendar/MobileCalendar";
+import useMediaQuery from "../../hooks/useMedia";
+const DateComponent = () => {
+  const startDateString = useSelector((state)=>state.Itinerary.start_date);
+  const endDateString = useSelector((state)=>state.Itinerary.end_date);
+  const isDesktop = useMediaQuery("(min-width:767px)");
+  const [date, setDate] = useState({
+    type: "fixed",
+    start_date: startDateString ? new Date(startDateString) : null,
+    end_date: endDateString ? new Date(endDateString) : null,
+    month: "",
+    duration: ""
+  });
+  const handleOnCalenderApplyDates = (values) => {
+    setDate({
+      ...date,
+      start_date: values.start,
+      end_date: values.end
+    });
+  }
+  const [showCalendar, setShowCalendar] = useState(false);
+  const SetDateType = (value) => {
+    setDate({
+      ...date,
+      type: value
+    });
+  }
+  return (
+    <div>
+        <div>
+        <div>
+          <div className="Body2R_14 mb-[4px]">When</div>
+          <div className="relative w-full">
+            <StyledFigmaBox
+              value={
+                date.type === "fixed" ? (date.start_date && date.end_date
+                  ? `${getHumanDate(date.start_date.toLocaleDateString("en-CA").split("-").reverse().join("/"))} - ${getHumanDate(date.end_date.toLocaleDateString("en-CA").split("-").reverse().join("/"))}`
+                  : "") : date.type === "flexible" ? (new Date(date.month).toLocaleString("default", { month: "long" }) + ", " + date.duration + " days") : date.duration + " days"
+              }
+              placeholder="Select dates"
+              className={`cursor-pointer w-full pr-10  Body2M_14`}
+              onClick={() => setShowCalendar(true)}
+              readOnly
+            />
+            <Image
+              src="/calendar.svg"
+              width={20}
+              height={20}
+              className="absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none"
+              alt="calendar"
+            />
+          </div>
+        </div>
+      </div>
+
+      {isDesktop ? <ModalWithBackdrop
+        centered
+        show={showCalendar}
+        mobileWidth="100%"
+        backdrop
+        closeIcon={true}
+        onHide={() => setShowCalendar(false)}
+        borderRadius={"12px"}
+        animation={false}
+        backdropStyle={{ backgroundColor: "rgba(0,0,0,0.4)", backdropFilter: "blur(1px)" }} // <- add this
+      >
+        <AirbnbCalendar
+          valueStart={date.start_date}
+          valueEnd={date.end_date}
+          onChangeDate={handleOnCalenderApplyDates}
+          setShowCalendar={setShowCalendar}
+          setDateType={SetDateType}
+          dateType={date.type}
+          date={date}
+        />
+      </ModalWithBackdrop> : <>
+        <BottomModal
+          show={showCalendar}
+          onHide={() => setShowCalendar(false)}
+          width="100%"
+          height="max-content"
+        >
+          <AirbnbCalendarMobile
+            valueStart={date.start_date}
+            valueEnd={date.end_date}
+            onChangeDate={handleOnCalenderApplyDates}
+            setShowCalendar={setShowCalendar}
+            setDateType={SetDateType}
+            dateType={date.type}
+            date={date}
+          />
+        </BottomModal>
+      </>}
+
+    </div>
+  )
+}
+
+export default DateComponent
