afb91f052d216624499273a94a9a7b806470ae2e
Aniket Singh <aniketsinghinform@gmail.com>
2025-11-24 05:46:13 +0530

    Cart Ui Update

--- components/revamp/home/NavigationMenu.jsx
+++ components/revamp/home/NavigationMenu.jsx
@@ -94,6 +94,7 @@ const NavigationMenu = (props) => {
 
   return (
     <>
+    <div className="w-100 bg-text-white"> 
       <nav className={styles.navigationMenu + " " + props.className + " max-ph:!p-md max-ph:shadow-soft"} role="navigation">
         <div className="hover-pointer" onClick={() => router.push("/")}>
           <Image src={TTW} alt="TTW Logo" priority />
@@ -177,6 +178,7 @@ const NavigationMenu = (props) => {
           } */}
         </div>
       </nav>
+      </div>
 
       {/* Mobile Sidebar Overlay */}
       <div
