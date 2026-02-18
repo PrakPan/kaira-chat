e3c6672098da4f8e09d1da20fad569bb946c8bd4
Prakhar Pandey <prakhar@thetarzanway.com>
2026-02-18 10:20:42 +0530

    Fix:: Added activity start time

--- components/drawers/poiDetails/ActivityDetails.jsx
+++ components/drawers/poiDetails/ActivityDetails.jsx
@@ -305,7 +305,7 @@ const ActivityDetails = (props) => {
       {props.data?.tags?.map((e, i) => (
         <div
           key={i}
-          className={`border-2 rounded-full px-2 py-1`}
+          className={`rounded-full px-2 py-1`}
           style={{ backgroundColor: colors[i % colors.length] }}
         >
           {e}
