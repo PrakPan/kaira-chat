b40b5f35be494d8e36506242199ab54d2c7d2439
Prakhar Pandey <prakhar@thetarzanway.com>
2026-02-27 11:12:26 +0530

    Fix:: Experience filters

--- components/styled-components/TailoredForm.js
+++ components/styled-components/TailoredForm.js
@@ -2,10 +2,13 @@ import styled from "styled-components";
 
 export const StyledHeading = styled.div`
   color: var(--text-colors-text-focused, #000);
+
+  /* Body/Body | Regular */
+  // font-family: Inter;
   font-size: 14px;
   font-style: normal;
   font-weight: 400;
-  line-height: 22px;
+  line-height: 22px; /* 157.143% */
 `;
 
 export const StyledContainer = styled.div`
@@ -21,25 +24,29 @@ export const StyledContainer = styled.div`
 
 export const StyledButton = styled.button`
   display: flex;
+  padding: 8px var(--XXL, 20px);
   justify-content: center;
   align-items: center;
-  height: 86px;
-  position: relative;
+
+  border-radius: 50px;
+  border: 1px solid var(--Text-Colors-Stroke, #e5e5e5);
+   ${({ clicked }) =>
+    clicked &&
+    `
+      border: 1px solid var(--primary-colors-primary-yellow-color, #f7e700);
+      background: var(--primary-colors-primary-yellow-color, #f7e700);
+    `}
+    &:hover {
+      background: #f7e700;
+    }
 `;
 
 export const StyledFlexWrap = styled.div`
   width: 100%;
-  display: grid;
-  grid-template-columns: repeat(2, 1fr);
-  gap: 12px;
-  
-  @media screen and (min-width: 768px) {
-    grid-template-columns: repeat(3, 1fr);
-    gap: 16px;
-  }
-  
-  @media screen and (min-width: 1024px) {
-    grid-template-columns: repeat(3, 1fr);
-    gap: 16px;
-  }
+  display: flex;
+  align-items: center;
+  align-content: center;
+  gap: 12px var(--L, 12px);
+  align-self: stretch;
+  flex-wrap: wrap;
 `;
