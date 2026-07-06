b2fa55eb2b5db31dc189a0ed9c21494e453ff929
Prakhar Pandey <prakhar@thetarzanway.com>
2026-07-06 14:18:46 +0530

    feat: skip rebuilding unchanged pages for faster deployment

--- pages/[continent]/[country]/[state]/[city]/index.js
+++ pages/[continent]/[country]/[state]/[city]/index.js
@@ -113,7 +113,7 @@ export async function getStaticPaths() {
     const res = await axiossearchallinstance.get("/all/?type=City");
     const data = res.data ?? [];
 
-    for (var i = 0; i < data.length; i++) {
+    for (var i = 0; i < 2; i++) {
       if (!data[i]?.path) continue;
       const pathArr = data[i].path.split("/");
       const [continentSlug, countrySlug, stateSlug, citySlug] = pathArr;
